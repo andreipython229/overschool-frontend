27b99019e2ed2ccae1955fba1cb59c9c1505b857
Zarazaka Mikalai <golmtz@gmail.com>
2022-07-30 17:14:47 +0300

    add modal addStudent

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,6 +1,6 @@
-import React, { ButtonHTMLAttributes, DetailedHTMLProps, FC } from "react";
+import React, { ButtonHTMLAttributes, DetailedHTMLProps, FC } from 'react';
 
-import styles from "./button.module.scss";
+import styles from './button.module.scss';
 
 type DefaultButtonPropsType = DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
@@ -9,18 +9,20 @@ type DefaultButtonPropsType = DetailedHTMLProps<
 
 type SuperButtonPropsT = DefaultButtonPropsType & {
   text: string;
-  variant?: "default" | "primary" | "disabled" | "registrationDisabled";
+  variant?: 'default' | 'primary' | 'disabled' | 'registrationDisabled' | 'secondary';
 };
 
-export const Button: FC<SuperButtonPropsT> = ({ text, variant = "default", ...restProps }) => {
+export const Button: FC<SuperButtonPropsT> = ({ text, variant = 'default', ...restProps }) => {
   let propsStyle = styles.btn_default;
 
-  if (variant === "primary") {
-    propsStyle += " " + styles.primary;
-  } else if (variant === "disabled") {
-    propsStyle += " " + styles.disabled;
-  } else if (variant === "registrationDisabled") {
-    propsStyle += " " + styles.registartionDisabled;
+  if (variant === 'primary') {
+    propsStyle += ' ' + styles.primary;
+  } else if (variant === 'disabled') {
+    propsStyle += ' ' + styles.disabled;
+  } else if (variant === 'registrationDisabled') {
+    propsStyle += ' ' + styles.registartionDisabled;
+  } else if (variant === 'secondary') {
+    propsStyle += ' ' + styles.secondary;
   } else {
     propsStyle = styles.btn_default;
   }
