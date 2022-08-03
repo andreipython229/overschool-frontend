1ed12b3353ee188dc60a59f860217124661041fc
Ramosok <Ramosok86@gmail.com>
2022-08-03 07:33:53 +0300

    add filters and add data-picker

--- src/components/common/IconSvg/IconSvg.tsx
+++ src/components/common/IconSvg/IconSvg.tsx
@@ -1,22 +1,23 @@
-import React, { FC } from "react";
+import React, { FC } from 'react'
 
 type IconSvgT = {
-  width: number;
-  height: number;
-  fill: string;
-  d: string;
-};
+  width: number
+  height: number
+  fill: string
+  d: string
+  viewBoxSize?: string
+}
 
-export const IconSvg: FC<IconSvgT> = ({ width, height, fill, d }) => {
+export const IconSvg: FC<IconSvgT> = ({ width, height, fill, d, viewBoxSize = ' 0 0 20 20' }) => {
   return (
     <svg
       width={width}
       height={height}
-      viewBox="0 0 20 20"
+      viewBox={viewBoxSize}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path d={d} fill={fill} />
     </svg>
-  );
-};
+  )
+}
--- src/components/common/SelectDropDown/SelectDropDown.tsx
+++ src/components/common/SelectDropDown/SelectDropDown.tsx
@@ -1,88 +1,118 @@
-import React, { useEffect, useState } from "react";
-import { IconSvg } from "../IconSvg/IconSvg";
-import { initialDropDownList } from "../../../constants/dropDownList";
-import { arrowIcon, triangleDownDownIcon } from "../../../constants/iconSvgConstants";
-import styles from "./select_drop_down.module.scss";
+import React, { FC, memo, useEffect, useState } from 'react'
+import { IconSvg } from '../IconSvg/IconSvg'
+import { initialDropDownList } from '../../../constants/dropDownList'
+import { arrowIcon, triangleDownDownIcon } from '../../../constants/iconSvgConstants'
+import styles from './select_drop_down.module.scss'
+import { arrowUsers } from '../../../mockData/mockData'
 
 type dropDownItem = {
-  id: number | string;
+  id: number | string
   icon: {
-    width: number;
-    height: number;
-    fill: string;
-    d: string;
-  };
-  title: string;
-  bg: any;
-};
+    width: number
+    height: number
+    fill: string
+    d: string
+  }
+  title: string
+  bg: any
+  viewBoxSize?: string
+}
 
-const selectTheJobStatus = "ВЫБЕРИТЕ СТАТУС РАБОТЫ";
+type SelectDropDownT = {
+  arrowUsersState: any
+  setArrowUsersState: any
+}
 
-export const SelectDropDown = () => {
-  const [toggleDropDown, setToggleDropDown] = useState(false);
-  const [headerDropDown, setHeaderDropDown] = useState<dropDownItem>(initialDropDownList[0]);
-  const [dropDownList, setDropDownList] = useState<dropDownItem[]>([]);
+const selectTheJobStatus = 'ВЫБЕРИТЕ СТАТУС РАБОТЫ'
 
-  const handleDropDown = () => {
-    setToggleDropDown(!toggleDropDown);
-  };
+export const SelectDropDown: FC<SelectDropDownT> = memo(
+  ({ arrowUsersState, setArrowUsersState }) => {
+    const [toggleDropDown, setToggleDropDown] = useState(false)
+    const [headerDropDown, setHeaderDropDown] = useState<dropDownItem>(initialDropDownList[0])
+    const [dropDownList, setDropDownList] = useState<dropDownItem[]>([])
 
-  const handleChangeStatus =
-    ({ title }: any) =>
-    () => {
-      const changeFilterStatusList = initialDropDownList.find(
-        (item: dropDownItem): boolean => item.title === title,
-      );
-      setHeaderDropDown(changeFilterStatusList || initialDropDownList[0]);
-      const changeDropDownList = initialDropDownList.filter(
-        (item: dropDownItem) => item.title !== title,
-      );
-      setDropDownList(changeDropDownList);
-      setToggleDropDown(false);
-    };
+    const handleDropDown = () => {
+      setToggleDropDown(!toggleDropDown)
+    }
 
-  useEffect(() => {
-    const defaultDropDownHeader = initialDropDownList.find(
-      (item: dropDownItem) => item.title === "Все статусы",
-    );
-    setHeaderDropDown(defaultDropDownHeader || initialDropDownList[0]);
-    const defaultDropDownList = initialDropDownList.filter(
-      (item: dropDownItem) => item.title !== "Все статусы",
-    );
-    setDropDownList(defaultDropDownList);
-  }, []);
+    const handleChangeStatus =
+      ({ title }: any) =>
+      () => {
+        const changeFilterStatusList = initialDropDownList.find(
+          (item: dropDownItem): boolean => item.title === title,
+        )
+        setHeaderDropDown(changeFilterStatusList || initialDropDownList[0])
+        const changeDropDownList = initialDropDownList.filter(
+          (item: dropDownItem) => item.title !== title,
+        )
+        setDropDownList(changeDropDownList)
+        if (title === 'Все статусы') {
+          setArrowUsersState(arrowUsers)
+        } else {
+          const newArrowUserState = arrowUsers.filter(
+            ({ status }: any) => status.toLowerCase().trim() === title.toLowerCase(),
+          )
+          setArrowUsersState(newArrowUserState)
+        }
 
-  return (
-    <>
-      <p
-        onClick={handleDropDown}
-        className={`${styles[headerDropDown.bg]} ${styles.header_dropdown_menu}`}
-      >
-        <IconSvg
-          width={headerDropDown.icon.width}
-          height={headerDropDown.icon?.height}
-          fill={headerDropDown.icon?.fill}
-          d={headerDropDown.icon?.d}
-        />
-        {headerDropDown?.title}
-        <span className={toggleDropDown ? styles.rotate_arrow : ""}>
-          <IconSvg width={25} height={25} fill="#9A9A9A" d={arrowIcon} />
-        </span>
-      </p>
-      {toggleDropDown && (
-        <div className={styles.drop_down_item_container}>
-          <div className={styles.triangle}>
-            <IconSvg width={30} height={30} fill="#FFFFFF" d={triangleDownDownIcon} />
-          </div>
-          <p>{selectTheJobStatus}</p>
-          {dropDownList.map(({ id, icon, title }) => (
-            <div onClick={handleChangeStatus({ title })} className={styles.drop_down_item} key={id}>
-              <IconSvg width={icon.width} height={icon.height} d={icon.d} fill={icon.fill} />
-              <span>{title}</span>
+        setToggleDropDown(false)
+      }
+
+    useEffect(() => {
+      const defaultDropDownHeader = initialDropDownList.find(
+        (item: dropDownItem): boolean => item.title === 'Все статусы',
+      )
+      setHeaderDropDown(defaultDropDownHeader || initialDropDownList[0])
+      const defaultDropDownList = initialDropDownList.filter(
+        (item: dropDownItem) => item.title !== 'Все статусы',
+      )
+      setDropDownList(defaultDropDownList)
+    }, [])
+
+    return (
+      <div>
+        <p
+          onClick={handleDropDown}
+          className={`${styles[headerDropDown.bg]} ${styles.header_dropdown_menu}`}
+        >
+          <IconSvg
+            width={headerDropDown.icon.width}
+            height={headerDropDown.icon?.height}
+            fill={headerDropDown.icon?.fill}
+            d={headerDropDown.icon?.d}
+            viewBoxSize={headerDropDown.viewBoxSize}
+          />
+
+          {headerDropDown?.title}
+          <span className={toggleDropDown ? styles.rotate_arrow : ''}>
+            <IconSvg width={25} height={25} fill="#9A9A9A" d={arrowIcon} viewBoxSize="0 0 12 15" />
+          </span>
+        </p>
+        {toggleDropDown && (
+          <div className={styles.drop_down_item_container}>
+            <div className={styles.triangle}>
+              <IconSvg
+                width={30}
+                height={30}
+                fill="#FFFFFF"
+                d={triangleDownDownIcon}
+                viewBoxSize="0 0 20 20"
+              />
             </div>
-          ))}
-        </div>
-      )}
-    </>
-  );
-};
+            <p>{selectTheJobStatus}</p>
+            {dropDownList.map(({ id, icon, title }) => (
+              <div
+                onClick={handleChangeStatus({ title })}
+                className={styles.drop_down_item}
+                key={id}
+              >
+                <IconSvg width={icon.width} height={icon.height} d={icon.d} fill={icon.fill} />
+                <span>{title}</span>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    )
+  },
+)
