d981741fad734274017918bca26641646a39d730
Maria Burdo <masha.burdo@gmail.com>
2022-08-17 13:10:18 +0300

    адаптировала под мобилки и планшеты
    (cherry picked from commit dc1f1bea20a30d807758862a2461b7b6dd65a7e3)

--- src/index.tsx
+++ src/index.tsx
@@ -21,7 +21,7 @@ root.render(
     <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={<p>Loading...</p>} persistor={persistor}>
-          {screenWidth >= 480 ? <App /> : <AppMobile />}
+          {screenWidth >= 1025 ? <App /> : <AppMobile />}
         </PersistGate>
       </Provider>
     </BrowserRouter>
