f8ca7bf5b1e33dd7d2f152c9b753ba916e07b0e8
Maria Burdo <masha.burdo@gmail.com>
2022-08-23 13:23:18 +0300

    добавила динамическую подгрузку курсов на мобилки

--- src/MobilePages/MobileCoursesPage/MobileCoursesPage.tsx
+++ src/MobilePages/MobileCoursesPage/MobileCoursesPage.tsx
@@ -1,46 +1,56 @@
-import React, { ChangeEvent, useState } from 'react'
+import React, {ChangeEvent, FC, memo, useEffect, useState} from 'react'
 import styles from './mobileCoursesPage.module.scss'
 import { CourseSearchInput } from 'MobilePages/MobileCoursesPage/CourseSearchInput/CourseSearchInput'
+import {CoursesT, getCourses} from '../../store/redux/courses/slice'
 import { MobileCourseBlock } from 'MobilePages/MobileCoursesPage/MobileCourseBlock/MobileCourseBlock'
+import {useAppDispatch, useAppSelector} from "../../store/hooks";
+import {allCoursesSelector} from "../../selectors";
+import {useFetchCoursesQuery} from "../../api/coursesServices";
+
+
 
 export const MobileCoursesPage = () => {
-  const [searchValue, setSearchValue] = useState<string>('')
+    const [searchValue, setSearchValue] = useState<string>('')
+    const dispatch = useAppDispatch()
 
-  const onChangeSearchValue = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(e.currentTarget.value)
-  }
-  return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.header_container}>
-          <img src="" alt="" />
-          <div className={styles.header_container_title}>
-            <span>Онлайн-обучение</span>
-            <span>Название</span>
-            <span>Краткое описание</span>
-          </div>
+    const { courses } = useAppSelector(allCoursesSelector)
+    const { data: coursesList } = useFetchCoursesQuery('')
+
+    useEffect(() => {
+        dispatch(getCourses(coursesList))
+    }, [coursesList])
+    const onChangeSearchValue = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.currentTarget.value)
+    }
+
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <div className={styles.header_container}>
+                    <img src="" alt="" />
+                    <div className={styles.header_container_title}>
+                        <span>Онлайн-обучение</span>
+                        <span>Название</span>
+                        <span>Краткое описание</span>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.search}>
+                <CourseSearchInput searchValue={searchValue} onChangeSearchValue={onChangeSearchValue} />
+            </div>
+            <span className={styles.title}>Мои курсы</span>
+            <div className={styles.course}>
+                {courses &&
+                    courses?.map((course: CoursesT) => (
+                        <MobileCourseBlock
+                            key={course.course_id}
+                            name={course.name}
+                            progress={'58'}
+                            desc={course.description}
+                        />
+                    ))}
+            </div>
         </div>
-      </div>
-      <div className={styles.search}>
-        <CourseSearchInput searchValue={searchValue} onChangeSearchValue={onChangeSearchValue} />
-      </div>
-      <span className={styles.title}>Мои курсы</span>
-      <div className={styles.course}>
-        <MobileCourseBlock
-          name={'The Way Python'}
-          progress={'13'}
-          desc={
-            ' Индивидуальное обучение по программе The Way Python! Ты станешь востребованным IT-разработчиком! У тебя 100% все получится! Здесь и сейчас!'
-          }
-        />
-        {/*<MobileCourseBlock*/}
-        {/*  name={'The Way Frontend'}*/}
-        {/*  progress={'58'}*/}
-        {/*  desc={*/}
-        {/*    ' Индивидуальное обучение по программе The Way Frontend! Ты станешь востребованным IT-разработчиком! У тебя 100% все получится! Здесь и сейчас!'*/}
-        {/*  }*/}
-        {/*/>*/}
-      </div>
-    </div>
-  )
+    )
 }
