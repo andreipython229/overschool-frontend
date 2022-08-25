ca48a7787f02b7abd1d919bc1e9d7763ff91818c
Pavlovich Dmitry <pavlovichdima1986@gmail.com>
2022-08-25 15:41:06 +0300

    fix courses page

--- src/Pages/Courses/Previous/CoursePrevious/index.tsx
+++ src/Pages/Courses/Previous/CoursePrevious/index.tsx
@@ -14,23 +14,24 @@ export const CoursePrevious = () => {
 
   return (
     <div className={styles.previous}>
+      
       <img className={styles.background_image_course} src={editableCourse?.photo_url} alt="" />
-
+      <div className={styles.previous_bcgrShadow}></div>
       <Link className={styles.back_all_course} to={`${Path.Courses}`}>
-        <svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="9" height="15" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M6.625 1.1875L1.3125 6.5L6.625 11.8125" stroke="#BA75FF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
         <span>Все курсы</span>
       </Link>
+      <div className={styles.previous_onlineCourses}>Онлайн-курс</div>
       <div className={styles.previous_title_name}>{editableCourse?.name}</div>
 
       <div className={styles.previous_btn}>
         <Button
           variant={'primary'}
           style={{
-            width: '220px',
-            fontSize: '10px',
-            fontWeight: '800',
+            fontSize: '12px',
+            fontWeight: '500',
           }}
           text={'Опубликовать курс'}
           // onClick={onClick}
