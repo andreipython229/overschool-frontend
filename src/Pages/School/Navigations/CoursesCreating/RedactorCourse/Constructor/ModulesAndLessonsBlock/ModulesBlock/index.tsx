f4854553eb8e9efa42b41459cee0bf66f893d36c
Pavlovich Dmitry <pavlovichdima1986@gmail.com>
2022-09-22 10:08:28 +0300

    completed work for modules

--- src/Pages/School/Navigations/CoursesCreating/RedactorCourse/Constructor/ModulesAndLessonsBlock/ModulesBlock/index.tsx
+++ src/Pages/School/Navigations/CoursesCreating/RedactorCourse/Constructor/ModulesAndLessonsBlock/ModulesBlock/index.tsx
@@ -2,7 +2,6 @@ import React, { ChangeEvent, FC, useEffect, useState } from 'react'
 import { IconSvg } from 'components/common/IconSvg/IconSvg'
 import { deleteIconPath } from '../../../../../../config/svgIconsPath'
 import { useDeleteModulesMutation, usePatchModulesMutation } from 'api/modulesServices'
-
 import { formDataConverter } from 'utils/formDataConverter'
 import { showModal } from 'store/redux/modal/slice'
 import { getSectionId } from 'store/redux/modules/slice'
@@ -11,6 +10,7 @@ import { useAppDispatch } from 'store/hooks'
 import styles from '../../constructor.module.scss'
 import stylesModules from '../ModulesBlock/modules_block.module.scss'
 
+import Lesson from 'assets/img/createCourse/lesson.svg'
 import { LessonsBlock } from '../LessonsBlock'
 
 type ModulesBlockT = {
@@ -23,7 +23,6 @@ type ModulesBlockT = {
 
 export const ModulesBlock: FC<ModulesBlockT> = ({ setLessonId, moduleName, lessonsList, id, setModalTypeClasses }) => {
   const dispatch = useAppDispatch()
-
   const [changeModuleName, setChangeModuleName] = useState<string>(moduleName)
 
   const [changeName] = usePatchModulesMutation()
@@ -51,28 +50,31 @@ export const ModulesBlock: FC<ModulesBlockT> = ({ setLessonId, moduleName, lesso
     if (formdata && id) {
       changeName({ formdata, id })
     }
-  }, [changeModuleName, dispatch])
+  }, [changeModuleName])
 
   return (
     <>
-      <div className={stylesModules.headerText}>
-        <span className={styles.redactorCourse_leftSide_desc_title + ' ' + stylesModules.test}>{changeModuleName} </span>
+      <ul className={styles.redactorCourse_leftSide_desc_headerText}>
+        <span className={styles.redactorCourse_leftSide_desc_headerText_title + ' ' + stylesModules.test}>{changeModuleName}
+        </span>
+        <span className={styles.redactorCourse_leftSide_desc_headerText_inputWrapper + ' ' + stylesModules.block}>
+          <input
+            type="text"
+            value={changeModuleName}
+            onChange={handleChangeModuleName}
+            className={styles.redactorCourse_leftSide_desc_headerText_inputWrapper_input}
+          />
+          <button className={styles.redactorCourse_leftSide_desc_headerText_inputWrapper_btn_delete} onClick={handleDeleteModule}>
+            <IconSvg width={19} height={19} viewBoxSize="0 0 19 19" path={deleteIconPath} />
+          </button>
+        </span>
 
-        <input
-          type="text"
-          value={changeModuleName}
-          onChange={handleChangeModuleName}
-          className={styles.redactorCourse_leftSide_desc_title + ' ' + stylesModules.block}
-        />
-        <button className={stylesModules.btn_delete_module} onClick={handleDeleteModule}>
-          <IconSvg width={19} height={19} viewBoxSize="0 0 19 19" path={deleteIconPath} />
-        </button>
-      </div>
       {lessonsList &&
         lessonsList.map(({ name, lesson_id }: any) => <LessonsBlock setLessonId={setLessonId} key={lesson_id} id={lesson_id} lessonsName={name} />)}
       <button className={styles.btn} onClick={handleOpenModalLesson}>
         + Занятие
       </button>
+      </ul>
     </>
   )
 }
