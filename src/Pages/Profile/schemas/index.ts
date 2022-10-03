b6dc80cfbdb15f1789d998dae83fd7f357c14e15
Милена Кот <milena290105@gmail.com>
2022-10-03 22:32:14 +0300

    fix profile

--- src/Pages/Profile/schemas/index.ts
+++ src/Pages/Profile/schemas/index.ts
@@ -2,14 +2,14 @@ import * as yup from 'yup'
 
 export const userDataSchema = yup.object().shape({
   email: yup.string().email('Invalid email address').trim().max(128, 'Максимально допустимое значение'),
-  avatar: yup.string(),
+  // avatar: yup.string(),
   last_name: yup.string().trim().min(1, 'Это поле не может быть пустым').max(150, 'Максимально допустимое значение'),
   first_name: yup.string().trim().min(1, 'Это поле не может быть пустым').max(150, 'Максимально допустимое значение'),
   phone: yup
     .string()
-    .matches(/^[+\d](?:.*\d)?$/, { message: 'Invalid phone number' })
+    // .matches(/^[+\d](?:.*\d)?$/, { message: 'Invalid phone number' })
     .trim(),
   city: yup.string().trim().min(1, 'Это поле не может быть пустым').max(256, 'Максимально допустимое значение'),
   desc: yup.string().trim(),
-  sex: yup.string().oneOf(['male', 'female']),
+  sex: yup.string().oneOf(['М', 'Ж']),
 })
