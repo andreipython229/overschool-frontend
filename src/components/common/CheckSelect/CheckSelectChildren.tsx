0f76ee996fde57708f75f4fb32cfb48695472daf
Pavlovich Dmitry <pavlovichdima1986@gmail.com>
2022-10-10 17:06:03 +0300

    fix modal

--- src/components/common/CheckSelect/CheckSelectChildren.tsx
+++ src/components/common/CheckSelect/CheckSelectChildren.tsx
@@ -14,10 +14,10 @@ export const CheckSelectChildren: FC<CheckSelectChildrenPropsT> = memo(({ text }
     setCheckbox(!checkbox)
   }
   return (
-    <div style={checkbox ? { backgroundColor: '#F5EBFF' } : { backgroundColor: '#F6F6F6' }} className={styles.selectChildren}>
+    <div style={checkbox ? { backgroundColor: '#F5EBFF', margin: '20px 0 20px 0' } : { backgroundColor: '#F6F6F6', margin: '20px 0 20px 0' } } className={styles.selectChildren}>
       <div className={styles.selectChildren_content}>
         <Checkbox id={'lesson'} name={'lesson'} checked={checkbox} onChange={handlerCheckbox} />
-        <div>
+        <div className={styles.selectChildren_content_title}>
           <span>{text}</span>
           <div>
             <IconSvg width={21} height={21} viewBoxSize="0 0 21 21" path={checkSelectChildIconPath} />
