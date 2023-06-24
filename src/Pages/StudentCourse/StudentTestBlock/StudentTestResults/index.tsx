da45e2acdcc724041a22da0cb2e6a94b40171ea6
aleksejpetrovec <neksomg123@gmail.com>
2023-06-24 13:06:27 +0300

    completed lesson tests services;

--- src/Pages/StudentCourse/StudentTestBlock/StudentTestResults/index.tsx
+++ src/Pages/StudentCourse/StudentTestBlock/StudentTestResults/index.tsx
@@ -3,37 +3,45 @@ import {Button} from "../../../../components/common/Button/Button";
 import {useSendTestResultsMutation} from "../../../../api/userTestService";
 
 interface TestResultProps {
-  results: { [key: number]: boolean | string };
-  test: number | string;
-  user: any;
+    results: { [key: number]: boolean | string };
+    test: number | string;
+    user: any;
 }
 
-export const StudentTestResults: React.FC<TestResultProps> = ({ results , test, user}) => {
-  const percentage = () => {
-      const totalQuestions = Object.keys(results).length;
-      const correctAnswers = Object.values(results).filter(answer => answer).length;
-      return (
-          correctAnswers / totalQuestions * 100
-      )
-  }
+export const StudentTestResults: React.FC<TestResultProps> = ({results, test, user}) => {
+    const [sendTestResults, {isLoading, isError, isSuccess}] = useSendTestResultsMutation();
 
-  const sendResults = () => {
-      useSendTestResultsMutation();
-  };
+    const percentage = () => {
+        const totalQuestions = Object.keys(results).length;
+        const correctAnswers = Object.values(results).filter(answer => answer).length;
+        return (
+            correctAnswers / totalQuestions * 100
+        )
+    }
+    const testResults = {
+        success_percent: percentage(),
+        status: percentage() > 80 ? 'П': 'Н',
+        test: test,
+        user: user
+    }
+
+    const handleSendResults = (body: {[key: string]: any}) => {
+        sendTestResults(body);
+    };
 
 
-  return (
-    <div>
-      <h2>Результаты теста</h2>
-      {Object.entries(results).map(([questionId, isCorrectAnswer]) => (
-        <div key={questionId}>
-          <p>Вопрос ID: {questionId}</p>
-          <p>Правильный ответ: {isCorrectAnswer ? 'Да' : 'Нет'}</p>
+    return (
+        <div>
+            <h2>Результаты теста</h2>
+            {Object.entries(results).map(([questionId, isCorrectAnswer]) => (
+                <div key={questionId}>
+                    <p>Вопрос ID: {questionId}</p>
+                    <p>Правильный ответ: {isCorrectAnswer ? 'Да' : 'Нет'}</p>
+                </div>
+            ))}
+            <h2>Правильных ответов: {percentage()}%!</h2>
+            <h2>Отправить результаты теста?</h2>
+            <Button text={'Отправить результаты'} onClick={() => handleSendResults(testResults)}/>
         </div>
-      ))}
-        <h2>Процент правильных ответов: {percentage()}!</h2>
-        <h2>Отправить результаты теста?</h2>
-        <Button text={'Отправить результаты'} onClick={sendResults}/>
-    </div>
-  );
+    );
 };
