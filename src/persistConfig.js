7480bbac345e5fca35b8c6f7a6c0eb41d893d933
aleksejpetrovec <neksomg123@gmail.com>
2023-07-25 04:18:39 +0300

    добавлен функционал + роутинг к бэку, зависящий от выбора школы;

--- src/persistConfig.js
+++
@@ -1,9 +0,0 @@
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-import { rootReducer } from './store/redux/store';
-
-const persistConfig = {
-  key: 'root',
-  storage,
-};
