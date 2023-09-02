9bb59611a0f513c5a491d3decbca20ff1c6bf320
overschool2 <rbtechologics@mail.ru>
2023-09-02 18:38:31 +0300

    ProgressBar
    	modified:   src/Pages/StudentCourse/StudentCourseHeader/index.tsx
    	modified:   src/Pages/StudentCourse/StudentCourseHeader/student_course_header.module.scss

--- src/Pages/StudentCourse/StudentCourseHeader/index.tsx
+++ src/Pages/StudentCourse/StudentCourseHeader/index.tsx
@@ -89,10 +89,8 @@ export const StudentCourseHeader: FC = () => {
 
                                 // Rotation of path and trail, in number of turns (0-1)
                                 rotation: 0.25, 
-
                                 // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
                                 strokeLinecap: 'butt', 
-                                
                                 // Text size
                                 textSize: '20px',
                                 // How long animation takes to go from one percentage to another, in seconds
