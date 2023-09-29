4febf171e542cda426647d359c57d9f0d93bdf91
aleksejpetrovec <rbtechologics@mail.ru>
2023-09-29 19:12:57 +0300

    страница регистрации новой школы доведена до ума;

--- src/Pages/CreateNewSchool/CreateNewSchool.tsx
+++ src/Pages/CreateNewSchool/CreateNewSchool.tsx
@@ -1,130 +1,187 @@
-import {Button} from '../../components/common/Button/Button'
+import { Button } from '../../components/common/Button/Button'
 import styles from './createNewSchool.module.scss'
-import {Path} from "../../enum/pathE";
-import {isSecurity, unSecurity} from "../../assets/img/common";
-import {InputAuth} from "../../components/common/Input/InputAuth/InputAuth";
-import {useFormik} from "formik";
-import {useState} from "react";
-import {generatePath, useNavigate} from "react-router-dom";
-import { useCreateSchoolOwnerMutation } from 'api/schoolCreationService';
+import { Path } from '../../enum/pathE'
+import { isSecurity, unSecurity } from '../../assets/img/common'
+import { InputAuth } from '../../components/common/Input/InputAuth/InputAuth'
+import { useFormik } from 'formik'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useCreateSchoolOwnerMutation } from 'api/schoolCreationService'
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
+import { SimpleLoader } from 'components/Loaders/SimpleLoader'
+import { validatePhone } from 'utils/validatePhone'
+import { validateEmail } from 'utils/validateEmail'
 
 export const CreateNewSchool = () => {
-    const [security, setSecurity] = useState<boolean>(true)
-    const [createOwner] = useCreateSchoolOwnerMutation()
-    const navigate = useNavigate()
+  const [security, setSecurity] = useState<boolean>(true)
+  const [createOwner, { isSuccess, isLoading }] = useCreateSchoolOwnerMutation()
+  const [open, setOpen] = useState(false)
+  const navigate = useNavigate()
+  const [error, setError] = useState<string>('')
 
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            phone_number: '',
-            password: '',
-            password_confirmation: '',
-        },
-        onSubmit: () => {
-            const userData = formik.values
-            console.log('data: ', userData)
-            createOwner(userData).unwrap().then(() => {navigate(generatePath(Path.ChooseSchool))})
-        },
-    });
+  const handleClose = () => {
+    setOpen(false)
+    navigate(Path.InitialPage)
+  }
 
-    const changeSecurityStatus = () => {
-        setSecurity(!security)
+  const formik = useFormik({
+    initialValues: {
+      school_name: '',
+      email: '',
+      phone_number: '',
+      password: '',
+      password_confirmation: '',
+    },
+    onSubmit: async () => {
+      if (
+        formik.values.school_name.length > 0 &&
+        formik.values.password === formik.values.password_confirmation &&
+        validatePhone(formik.values.phone_number) &&
+        validateEmail(formik.values.email)
+      ) {
+        const userData = formik.values
+        await createOwner(userData)
+          .unwrap()
+          .catch(response => {
+            if (response.status === 400) {
+              setError(response.data)
+            }
+          })
+      } else {
+        setError('Проверьте правильность введенных данных')
+      }
+    },
+  })
+
+  useEffect(() => {
+    if (isSuccess) {
+      setOpen(true)
     }
+  }, [isSuccess, isLoading])
 
-    return (
-        <section className={styles.newCoursePage}>
-            <div className={styles.newCoursePage_logoWrapper}>
-                <div className={styles.newCoursePage_logoWrapper_container}>
-                    <svg width="230" height="103" viewBox="0 0 230 103" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M174.192 -0.000967405C174.904 -0.00278475 175.589 0.265959 176.117 0.752598L210.92 32.9128L229.043 49.3535C230.294 50.4876 230.322 52.4704 229.104 53.6405L216.612 65.6498L181.693 101.708C181.157 102.26 180.429 102.571 179.669 102.573L136.491 102.684C133.936 102.69 132.661 99.5321 134.482 97.7044L179.251 52.7929C179.93 52.0414 179.888 51.7491 179.251 51.0602L170.272 41.4769L131.505 5.15145C129.594 3.36052 130.832 0.110076 133.429 0.1034L174.192 -0.000967405Z"
-                            fill="url(#paint0_linear_1323_4107)"
-                        />
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M68.4455 74.6878C67.4499 75.8543 67.5243 77.6104 68.614 78.6856L91.5215 101.278C92.7028 102.443 94.6034 102.352 95.6726 101.08L176.552 4.79767C178.139 2.90863 176.818 -0.00723445 174.377 -0.000966474L133.392 0.103958C132.569 0.106072 131.787 0.470392 131.246 1.10331L68.4455 74.6878Z"
-                            fill="url(#paint1_linear_1323_4107)"
-                        />
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M21.0084 26.3669L46.0685 1.16134C46.6015 0.620036 47.3227 0.315242 48.0754 0.313313L90.3417 0.205088C92.8834 0.198561 94.1643 3.32724 92.3713 5.16357L48.7023 49.8892C48.3891 50.2232 48.3837 50.4334 48.7023 50.8181L95.3713 97.8173C97.1716 99.6447 95.9061 102.778 93.3651 102.785L90.656 102.792C90.6366 102.792 90.6175 102.792 90.5981 102.792L52.2512 102.891C52.2328 102.891 52.2145 102.891 52.1962 102.89L51.538 102.892C50.7604 102.894 50.0157 102.572 49.4761 102.002L0.798224 50.4967C-0.289989 49.3451 -0.262078 47.5144 0.86075 46.3981L21.0084 26.3669Z"
-                            fill="url(#paint2_linear_1323_4107)"
-                        />
-                        <defs>
-                            <linearGradient id="paint0_linear_1323_4107" x1="140.844" y1="0.0844086" x2="209.849"
-                                            y2="59.4753" gradientUnits="userSpaceOnUse">
-                                <stop stopColor="#9228FF"/>
-                                <stop offset="1" stopColor="#EC40FF"/>
-                            </linearGradient>
-                            <linearGradient id="paint1_linear_1323_4107" x1="150.641" y1="0.0597811" x2="67.4794"
-                                            y2="103.714" gradientUnits="userSpaceOnUse">
-                                <stop stopColor="#CD99FF"/>
-                                <stop offset="1" stopColor="#6D1CBA"/>
-                            </linearGradient>
-                            <linearGradient id="paint2_linear_1323_4107" x1="8.10532" y1="38.9434" x2="70.8075"
-                                            y2="102.291" gradientUnits="userSpaceOnUse">
-                                <stop stopColor="#61A3ED"/>
-                                <stop offset="1" stopColor="#5048D8"/>
-                            </linearGradient>
-                        </defs>
-                    </svg>
-                    <p className={styles.newCoursePage_logoWrapper_container_title}>OVERSCHOOL</p>
-                </div>
-            </div>
-            <div className={styles.newCoursePage_formWrapper}>
-                <form className={styles.newCoursePage_formWrapper_form} onSubmit={formik.handleSubmit}>
-                    <p className={styles.newCoursePage_formWrapper_form_title}>Регистрация владельца школы</p>
-                    {/*<p className={styles.newCoursePage_formWrapper_form_help}>Введите свои данные</p>*/}
-                    <div className={styles.newCoursePage_formWrapper_form_eMailWrapper}>
-                        <p className={styles.newCoursePage_formWrapper_form_eMailWrapper_title}>E-mail:</p>
-                        <InputAuth name={'email'} type={'text'} onChange={formik.handleChange}
-                                   value={formik.values.email} placeholder={'E-mail'}/>
-                    </div>
-                    <div className={styles.newCoursePage_formWrapper_form_passwordWrapper}>
-                        <p className={styles.newCoursePage_formWrapper_form_passwordWrapper_title}>Номер телефона:</p>
-                        <InputAuth
-                            name={'phone_number'}
-                            type={'tel'}
-                            onChange={formik.handleChange}
-                            value={formik.values.phone_number}
-                            placeholder={'Номер телефона'}
-                        />
-                    </div>
-                    <div className={styles.newCoursePage_formWrapper_form_passwordWrapper}>
-                        <p className={styles.newCoursePage_formWrapper_form_passwordWrapper_title}>Пароль:</p>
-                        <InputAuth name={'password'}
-                                   type={security ? 'password' : 'text'}
-                                   onChange={formik.handleChange}
-                                   value={formik.values.password}
-                                   placeholder={'Пароль'}
-                                   onClick={changeSecurityStatus}
-                                   icon={security ? isSecurity : unSecurity}
-                        />
-                    </div>
-                    <div className={styles.newCoursePage_formWrapper_form_passwordWrapper}>
-                        <p className={styles.newCoursePage_formWrapper_form_passwordWrapper_title}>Повторите пароль:</p>
-                        <InputAuth name={'password_confirmation'}
-                                   type={security ? 'password' : 'text'}
-                                   onChange={formik.handleChange}
-                                   value={formik.values.password_confirmation}
-                                   placeholder={'Повторите пароль'}
-                                   onClick={changeSecurityStatus}
-                                   icon={security ? isSecurity : unSecurity}
-                        />
-                    </div>
-                    <div className={styles.newCoursePage_formWrapper_form_btnCreateWrapper}>
-                        <Button text={'Создать свой проект'} variant={'create'} type={'submit'}/>
-                        <p className={styles.newCoursePage_formWrapper_form_btnCreateWrapper_help}>
-                            Уже есть свой аккаунт?
-                            <a href={Path.InitialPage}>Войти</a>
-                        </p>
-                    </div>
-                </form>
-            </div>
-        </section>
-    )
+  const changeSecurityStatus = () => {
+    setSecurity(!security)
+  }
+
+  return (
+    <section className={styles.newCoursePage}>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+        <DialogTitle id="alert-dialog-title">{'Регистрация произведена успешно!'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Поздравляем, Вы стали на шаг ближе к своей цели. Теперь вы можете авторизоваться под данными Вашей, только что созданной, учётной записи и
+            приступить к администрированию собственной школы!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button style={{ marginInlineEnd: '1em', marginBottom: '.5em' }} onClick={handleClose} text={'Окей'} />
+        </DialogActions>
+      </Dialog>
+      <div className={styles.newCoursePage_logoWrapper}>
+        <div className={styles.newCoursePage_logoWrapper_container}>
+          <svg width="230" height="103" viewBox="0 0 230 103" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M174.192 -0.000967405C174.904 -0.00278475 175.589 0.265959 176.117 0.752598L210.92 32.9128L229.043 49.3535C230.294 50.4876 230.322 52.4704 229.104 53.6405L216.612 65.6498L181.693 101.708C181.157 102.26 180.429 102.571 179.669 102.573L136.491 102.684C133.936 102.69 132.661 99.5321 134.482 97.7044L179.251 52.7929C179.93 52.0414 179.888 51.7491 179.251 51.0602L170.272 41.4769L131.505 5.15145C129.594 3.36052 130.832 0.110076 133.429 0.1034L174.192 -0.000967405Z"
+              fill="url(#paint0_linear_1323_4107)"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M68.4455 74.6878C67.4499 75.8543 67.5243 77.6104 68.614 78.6856L91.5215 101.278C92.7028 102.443 94.6034 102.352 95.6726 101.08L176.552 4.79767C178.139 2.90863 176.818 -0.00723445 174.377 -0.000966474L133.392 0.103958C132.569 0.106072 131.787 0.470392 131.246 1.10331L68.4455 74.6878Z"
+              fill="url(#paint1_linear_1323_4107)"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M21.0084 26.3669L46.0685 1.16134C46.6015 0.620036 47.3227 0.315242 48.0754 0.313313L90.3417 0.205088C92.8834 0.198561 94.1643 3.32724 92.3713 5.16357L48.7023 49.8892C48.3891 50.2232 48.3837 50.4334 48.7023 50.8181L95.3713 97.8173C97.1716 99.6447 95.9061 102.778 93.3651 102.785L90.656 102.792C90.6366 102.792 90.6175 102.792 90.5981 102.792L52.2512 102.891C52.2328 102.891 52.2145 102.891 52.1962 102.89L51.538 102.892C50.7604 102.894 50.0157 102.572 49.4761 102.002L0.798224 50.4967C-0.289989 49.3451 -0.262078 47.5144 0.86075 46.3981L21.0084 26.3669Z"
+              fill="url(#paint2_linear_1323_4107)"
+            />
+            <defs>
+              <linearGradient id="paint0_linear_1323_4107" x1="140.844" y1="0.0844086" x2="209.849" y2="59.4753" gradientUnits="userSpaceOnUse">
+                <stop stopColor="#9228FF" />
+                <stop offset="1" stopColor="#EC40FF" />
+              </linearGradient>
+              <linearGradient id="paint1_linear_1323_4107" x1="150.641" y1="0.0597811" x2="67.4794" y2="103.714" gradientUnits="userSpaceOnUse">
+                <stop stopColor="#CD99FF" />
+                <stop offset="1" stopColor="#6D1CBA" />
+              </linearGradient>
+              <linearGradient id="paint2_linear_1323_4107" x1="8.10532" y1="38.9434" x2="70.8075" y2="102.291" gradientUnits="userSpaceOnUse">
+                <stop stopColor="#61A3ED" />
+                <stop offset="1" stopColor="#5048D8" />
+              </linearGradient>
+            </defs>
+          </svg>
+          <p className={styles.newCoursePage_logoWrapper_container_title}>OVERSCHOOL</p>
+        </div>
+      </div>
+      <div className={styles.newCoursePage_formWrapper}>
+        <form className={styles.newCoursePage_formWrapper_form} onSubmit={formik.handleSubmit}>
+          <p className={styles.newCoursePage_formWrapper_form_title}>Регистрация владельца школы</p>
+          {/*<p className={styles.newCoursePage_formWrapper_form_help}>Введите свои данные</p>*/}
+          <div className={styles.newCoursePage_formWrapper_form_eMailWrapper}>
+            {error && <p style={{ color: 'red', marginTop: '.5em' }}>{error}</p>}
+            <p className={styles.newCoursePage_formWrapper_form_eMailWrapper_title}>Введите название Вашей школы:</p>
+            <InputAuth
+              name={'school_name'}
+              type={'text'}
+              onChange={formik.handleChange}
+              value={formik.values.school_name}
+              placeholder={'Название школы'}
+            />
+          </div>
+          <div className={styles.newCoursePage_formWrapper_form_eMailWrapper}>
+            <p className={styles.newCoursePage_formWrapper_form_eMailWrapper_title}>E-mail:</p>
+            <InputAuth name={'email'} type={'text'} onChange={formik.handleChange} value={formik.values.email} placeholder={'E-mail'} />
+          </div>
+          <div className={styles.newCoursePage_formWrapper_form_passwordWrapper}>
+            <p className={styles.newCoursePage_formWrapper_form_passwordWrapper_title}>Номер телефона:</p>
+            <InputAuth
+              name={'phone_number'}
+              type={'tel'}
+              onChange={formik.handleChange}
+              value={formik.values.phone_number}
+              placeholder={'Номер телефона'}
+            />
+          </div>
+          <div className={styles.newCoursePage_formWrapper_form_passwordWrapper}>
+            <p className={styles.newCoursePage_formWrapper_form_passwordWrapper_title}>Пароль:</p>
+            <InputAuth
+              name={'password'}
+              type={security ? 'password' : 'text'}
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              placeholder={'Пароль'}
+              onClick={changeSecurityStatus}
+              icon={security ? isSecurity : unSecurity}
+            />
+          </div>
+          <div className={styles.newCoursePage_formWrapper_form_passwordWrapper}>
+            <p className={styles.newCoursePage_formWrapper_form_passwordWrapper_title}>Повторите пароль:</p>
+            <InputAuth
+              name={'password_confirmation'}
+              type={security ? 'password' : 'text'}
+              onChange={formik.handleChange}
+              value={formik.values.password_confirmation}
+              placeholder={'Повторите пароль'}
+              onClick={changeSecurityStatus}
+              icon={security ? isSecurity : unSecurity}
+            />
+          </div>
+          <div className={styles.newCoursePage_formWrapper_form_btnCreateWrapper}>
+            <Button
+              text={isLoading ? <SimpleLoader style={{ width: '2em', height: '2em' }} /> : 'Создать свой проект'}
+              variant={'create'}
+              type={'submit'}
+            />
+            <p className={styles.newCoursePage_formWrapper_form_btnCreateWrapper_help}>
+              Уже есть свой аккаунт?
+              <a href={Path.InitialPage}>Войти</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </section>
+  )
 }
