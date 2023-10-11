79a8249442f5ef5da0e7a154711ec784ce433986
Denis Sturchenko <developden@gmail.com>
2023-10-11 22:03:07 +0300

    tests fix

--- src/Pages/StudentCourse/StudentTestBlock/index.tsx
+++ src/Pages/StudentCourse/StudentTestBlock/index.tsx
@@ -1,10 +1,11 @@
-import {FC, useState} from 'react'
+import {FC, useState, useEffect} from 'react'
 import {StudentQuestion} from './StudentQuestion'
 
 import styles from './studentTestBlock.module.scss'
 import {StudentTestResults} from "./StudentTestResults";
 import {selectUser} from "../../../selectors";
 import {useSelector} from "react-redux";
+import {AnswersT} from "../../../components/AddQuestion";
 
 type questionListT = {
     attempt_count: number
@@ -14,6 +15,13 @@ type questionListT = {
     show_right_answers: boolean
     test: string
 }
+type QuestionT = {
+    question_type: keyof object
+    body: string
+    answers: AnswersT[]
+    question_id: number
+}
+
 
 export type AnswersType = {
     [key: string | number]: boolean | string
@@ -22,13 +30,42 @@ export type AnswersType = {
 export const StudentTestBlock: FC<any> = ({lesson}) => {
     const [numberTest, setNumberTest] = useState<number>(0)
     const [userAnswers, updateUserAnswers] = useState<AnswersType>({})
+
     const [testCompleted, setTestCompleted] = useState(false);
     const user = useSelector(selectUser);
+    const [questions, setQuestions] = useState(lesson.questions)
 
 
     const completeTest = () => {
         setTestCompleted(true);
     };
+
+    useEffect(() => {
+      if (lesson.random_questions) {
+        const shuffledQuestions = lesson.questions.map((question: QuestionT) => {
+          const shuffledAnswers = shuffleArray([...question.answers]);
+          return { ...question, answers: shuffledAnswers };
+        });
+        setQuestions(shuffleArray(shuffledQuestions));
+      }
+    }, [lesson]);
+
+    // Функция для перемешивания массива
+    function shuffleArray(array: any): any {
+      const newArray = array.slice();
+      for (let i = newArray.length - 1; i > 0; i -= 1) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+      }
+      return newArray;
+    }
+
+    useEffect(() => {
+        if (questions.length > 0 && numberTest >= questions.length) {
+          setNumberTest(0);
+        }
+      }, [questions, numberTest]);
+
 
     const nullQuestionTest = [{
         body: 'В данном тесте нет вопросов :(',
@@ -41,13 +78,13 @@ export const StudentTestBlock: FC<any> = ({lesson}) => {
 
     return (
         <div className={styles.wrapper} key={lesson.test}>
-            {lesson.questions.length > 0 ? (
+            {questions.length > 0 ? (
                 testCompleted ? (
                     <StudentTestResults results={userAnswers} test={lesson.test} user={user.userId}
-                                        success_prercent={''}/>
+                                        success_prercent={lesson.success_percent}/>
                 ) : (
-                    <StudentQuestion questions={lesson.questions[numberTest]}
-                                     length={lesson.questions} numberTest={numberTest}
+                    <StudentQuestion questions={questions[numberTest]}
+                                     length={questions} numberTest={numberTest}
                                      setNumberTest={setNumberTest} userAnswers={userAnswers}
                                      updateUserAnswers={updateUserAnswers} onCompleteTest={completeTest}/>
                 )
--- src/components/Modal/ModalTypeClasses/TestModal/config/checkboxData.ts
+++ src/components/Modal/ModalTypeClasses/TestModal/config/checkboxData.ts
@@ -1,16 +1,16 @@
 export const checkboxData = [
-  {
-    id: 'randomQuestions',
-    name: 'rndQuest',
-    span1: 'Случайный порядок вопросов',
-    span2: 'Вопросы будут задаваться в случайном порядк',
-  },
-  {
-    id: 'shuffleAnswers',
-    name: 'shuffleAnswer',
-    span1: 'Перемешивать варианты ответов',
-    span2: 'Варианты ответов будут отображаться в случайном порядке',
-  },
+  // {
+  //   id: 'randomQuestions',
+  //   name: 'rndQuest',
+  //   span1: 'Случайный порядок вопросов',
+  //   span2: 'Вопросы будут задаваться в случайном порядк',
+  // },
+  // {
+  //   id: 'shuffleAnswers',
+  //   name: 'shuffleAnswer',
+  //   span1: 'Перемешивать варианты ответов',
+  //   span2: 'Варианты ответов будут отображаться в случайном порядке',
+  // },
   {
     id: 'showCorrectAnswer',
     name: 'showCorrect',
