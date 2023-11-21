3e6e1c1745b2cb5b71b57823632f79b1e22c06e8
overschool2 <144776685+overschool2@users.noreply.github.com>
2023-11-21 14:44:55 +0300

    Корректировка

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -217,6 +217,7 @@ export const Header = memo(() => {
             </NavLink>
             <div className={styles.header_block}>
                 <React.Fragment>
+                {(role === RoleE.Admin && currentTariff) && <div>
                 <Tooltip title={'Статистика тарифа'}>
                         <div style={{ textDecoration: 'none' }} onClick={handleClick2}>
                         {(role === RoleE.Admin && currentTariff) && <a className={styles.tariffPlan}>
@@ -234,35 +235,8 @@ export const Header = memo(() => {
                     <Menu anchorEl={anchorEl2}
                         id="account-menu" open={open2}
                         onClose={handleClose2} onClick={handleClose2}
-                        PaperProps={{
-                            elevation: 0,
-                            sx: {
-                                overflow: 'visible',
-                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                                mt: 1.5,
-                                '& .MuiAvatar-root': {
-                                    width: 32,
-                                    height: 32,
-                                    ml: -0.5,
-                                    mr: 1,
-                                },
-                                '&:before': {
-                                    content: '""',
-                                    display: 'block',
-                                    position: 'absolute',
-                                    top: 0,
-                                    right: 14,
-                                    width: 10,
-                                    height: 10,
-                                    bgcolor: 'background.paper',
-                                    transform: 'translateY(-50%) rotate(45deg)',
-                                    zIndex: 0,
-                                },
-                            },
-                        }}
-                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                    >    <MenuItem>
+                    >    
+                    <MenuItem>
                             <span style={{ color: 'slategrey' }}> Курсов:</span><span style={{ color: '#BA75FF' }}>  {`${currentTariff?.number_of_courses}/${tariff?.number_of_courses || 'ꝏ'}`}</span><br/>
                         </MenuItem>
                         <MenuItem>
@@ -272,13 +246,14 @@ export const Header = memo(() => {
                             <span style={{ color: 'slategrey' }}> Студентов:</span><span style={{ color: '#BA75FF' }}>  {`${currentTariff?.students}/${tariff?.students || 'ꝏ'}`}</span>
                         </MenuItem>
                         <MenuItem onClick={goToChooseTariff}>
-                            <SvgIcon width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#708090" stroke-width="2" 
-                            stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line>
+                            <SvgIcon width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#708090" strokeWidth="2" 
+                            strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line>
                             <line x1="3" y1="18" x2="21" y2="18"></line>
                             </SvgIcon>
                             <Link to={Path.TariffPlans} style={{ color: 'slategrey' }}>Все тарифы</Link>
                         </MenuItem>
                     </Menu>
+                    </div>}
                     <Tooltip title={'Аккаунт пользователя'}>
                         <div style={{ textDecoration: 'none' }} onClick={handleClick}>
                             <div className={styles.header_block_user}>
@@ -310,34 +285,7 @@ export const Header = memo(() => {
                     <Menu anchorEl={anchorEl}
                         id="account-menu" open={open}
                         onClose={handleClose} onClick={handleClose}
-                        PaperProps={{
-                            elevation: 0,
-                            sx: {
-                                overflow: 'visible',
-                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                                mt: 1.5,
-                                '& .MuiAvatar-root': {
-                                    width: 32,
-                                    height: 32,
-                                    ml: -0.5,
-                                    mr: 1,
-                                },
-                                '&:before': {
-                                    content: '""',
-                                    display: 'block',
-                                    position: 'absolute',
-                                    top: 0,
-                                    right: 14,
-                                    width: 10,
-                                    height: 10,
-                                    bgcolor: 'background.paper',
-                                    transform: 'translateY(-50%) rotate(45deg)',
-                                    zIndex: 0,
-                                },
-                            },
-                        }}
-                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                       
                     >
                         <MenuItem onClick={goToProfile}>
                             <Avatar />
