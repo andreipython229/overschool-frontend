d5e3c063bcc2b62838f7da4d1a0823f1b1fa912e
aleksejpetrovec <rbtechologics@mail.ru>
2023-11-21 15:58:26 +0300

    правки по изменению названия школы;

--- src/Pages/Settings/Main/Main.tsx
+++ src/Pages/Settings/Main/Main.tsx
@@ -1,18 +1,20 @@
-import { useSetSchoolMutation, useFetchSchoolQuery } from "../../../api/schoolService";
+import { useSetSchoolMutation, useFetchSchoolQuery } from '../../../api/schoolService'
 import { ChangeEvent, memo, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import {Button} from 'components/common/Button/Button'
-import {Input} from 'components/common/Input/Input/Input'
+import { useNavigate } from 'react-router-dom'
+import { Button } from 'components/common/Button/Button'
+import { Input } from 'components/common/Input/Input/Input'
 
 import styles from '../superAdmin.module.scss'
-import { useAppDispatch } from "store/hooks";
-import { setSchoolName } from "store/redux/school/schoolSlice";
+import { useAppDispatch } from 'store/hooks'
+import { Path } from 'enum/pathE'
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
 
 export const Main = memo(() => {
-  const schoolId = localStorage.getItem("school_id")
+  const schoolId = localStorage.getItem('school_id')
+  const [alertOpen, setAlertOpen] = useState<boolean>(false)
 
-  const {data } = useFetchSchoolQuery(Number(schoolId))
-  const [updateDateSchoolName, { data: newName, isLoading, isSuccess }] = useSetSchoolMutation()
+  const { data } = useFetchSchoolQuery(Number(schoolId))
+  const [updateDateSchoolName, { data: newName, isLoading, isSuccess, isError }] = useSetSchoolMutation()
 
   const [name, setName] = useState<string>('')
   const [isNewName, setIsNewName] = useState<boolean>(false)
@@ -21,9 +23,10 @@ export const Main = memo(() => {
   const [url, setUrl] = useState<string>('')
   const [isNewUrl, setIsNewUrl] = useState<boolean>(false)
   const [oldUrl, setOldUrl] = useState<string>('')
-  
+  const [error, setError] = useState<string>()
+
   const dispatch = useAppDispatch()
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (data) {
@@ -35,6 +38,7 @@ export const Main = memo(() => {
   }, [data])
 
   const handleChangeSchoolName = (event: ChangeEvent<HTMLInputElement>) => {
+    setError('')
     setName(event.currentTarget.value)
   }
 
@@ -60,42 +64,83 @@ export const Main = memo(() => {
 
   const onChangeUrl = async () => {
     const formdata = new FormData()
-    formdata.append('name', name)
     formdata.append('offer_url', url)
     setIsNewUrl(false)
     await updateDateSchoolName({ formdata, id: Number(schoolId) })
   }
 
+  const handleCloseAlert = () => {
+    setAlertOpen(false)
+  }
+
+  const handleOpenAlert = () => {
+    setAlertOpen(true)
+  }
+
   useEffect(() => {
     if (isSuccess) {
-      dispatch(setSchoolName(name))
-      navigate(`/school/${name}/settings`)
+      navigate(Path.ChooseSchool)
     }
-  },[isSuccess])
+    if (isError) {
+      handleCloseAlert()
+      setError('Такое имя школы уже существует')
+    }
+  }, [isSuccess, isError])
 
   return (
     <div className={styles.wrapper_actions}>
       <div className={styles.main}>
         <div className={styles.main_title}>Основные настройки школы</div>
         <div className={styles.main_project}>Название школы</div>
-        <div className={styles.main_description} style={{color: 'red', padding: '0px', lineHeight: '1.5', opacity: '0.6', fontWeight: 'bold', fontSize: '12px'}}>
-          Название будет подставляться в url адресах относящихся к вашей школе.<br/>
-          Имейте ввиду, если вы делились с кем либо url ссылками на вашу школу - то после изменения по старым url адресам она будет не доступна!!!<br />
-          Частая смена не рекомендуется!!!</div>
-        <div>
-          <Input name={'name'} type={'text'} style={{width: '320px', paddingBottom: '10px'}} value={name} onChange={handleChangeSchoolName}/>
-          <Button onClick={onChangeProjectName} style={{width: '120px'}} variant={isNewName && !isLoading ? 'primary' : 'disabled'}
-                            text={'Сохранить'} disabled={!isNewName || isLoading} />
+        <div
+          className={styles.main_description}
+          style={{ color: 'red', padding: '0px', lineHeight: '1.5', opacity: '0.6', fontWeight: 'bold', fontSize: '12px' }}
+        >
+          Название будет подставляться в url адресах относящихся к вашей школе.
+          <br />
+          Имейте ввиду, если вы делились с кем либо url ссылками на вашу школу - то после изменения по старым url адресам она будет не доступна!!!
+          <br />
+          Частая смена не рекомендуется!!!
         </div>
-        <div><br/></div>
+        <div>
+          <Input name={'name'} type={'text'} style={{ width: '320px', paddingBottom: '10px' }} value={name} onChange={handleChangeSchoolName} />
+          {error && <p className={styles.error}>{error}</p>}
+          <Button
+            onClick={handleOpenAlert}
+            style={{ width: '120px' }}
+            variant={isNewName && !isLoading ? 'primary' : 'disabled'}
+            text={'Сохранить'}
+            disabled={!isNewName || isLoading}
+          />
+        </div>
+        <div>
+          <br />
+        </div>
         <hr />
         <div className={styles.main_project}>Ссылка на файл публичного договора оферты</div>
         <div>
-          <Input name={'name'} type={'text'} style={{width: '320px', paddingBottom: '10px'}} value={url} onChange={handleChangeUrl}/>
-          <Button onClick={onChangeUrl} style={{width: '120px'}} variant={isNewUrl && !isLoading ? 'primary' : 'disabled'}
-                            text={'Сохранить'} disabled={!isNewUrl || isLoading}/>
+          <Input name={'name'} type={'text'} style={{ width: '320px', paddingBottom: '10px' }} value={url} onChange={handleChangeUrl} />
+          <Button
+            onClick={onChangeUrl}
+            style={{ width: '120px' }}
+            variant={isNewUrl && !isLoading ? 'primary' : 'disabled'}
+            text={'Сохранить'}
+            disabled={!isNewUrl || isLoading}
+          />
         </div>
       </div>
+      <Dialog open={alertOpen} onClose={handleCloseAlert} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+        <DialogTitle id="alert-dialog-title">{`Вы действительно хотите изменить название школы "${oldName}" на "${name}"?`}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {' После нажатия на кнопку "Подтвердить", вы будете перенаправлены на страницу выбора школы, продолжить?'}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseAlert} text={'Отмена'} variant={'secondary'} />
+          <Button onClick={onChangeProjectName} autoFocus text={'Подтвердить'} variant={'primary'} />
+        </DialogActions>
+      </Dialog>
     </div>
   )
 })
