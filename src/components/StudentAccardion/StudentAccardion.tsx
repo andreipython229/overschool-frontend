34d61628ce64f25657188583c0655d06422a242c
aleksejpetrovec <rbtechologics@mail.ru>
2023-11-30 13:38:45 +0300

    исправлены баги с отметкой доступных уроков со стороны ученика;

--- src/components/StudentAccardion/StudentAccardion.tsx
+++ src/components/StudentAccardion/StudentAccardion.tsx
@@ -1,27 +1,26 @@
-import {FC, useState} from 'react'
+import { FC, memo, useState } from 'react'
 
-import {AccardionItem} from './AccardionItem/AccardionItem'
-import {sectionT, studentAccardioT} from '../../types/sectionT'
+import { AccardionItem } from './AccardionItem/AccardionItem'
+import { sectionT, studentAccardioT } from '../../types/sectionT'
 
 import styles from './studentAccardion.module.scss'
 
-export const StudentAccardion: FC<studentAccardioT> = ({modules}) => {
-    const [openIndex, setOpenIndex] = useState<number>(0)
+export const StudentAccardion: FC<studentAccardioT> = memo(({ modules }) => {
+  const [openIndex, setOpenIndex] = useState<number>(0)
 
-    const handleToggleOpen = (index: number) => {
-        if (openIndex === index) {
-            setOpenIndex(-1)
-        } else {
-            setOpenIndex(index)
-        }
+  const handleToggleOpen = (index: number) => {
+    if (openIndex === index) {
+      setOpenIndex(-1)
+    } else {
+      setOpenIndex(index)
     }
+  }
 
-    return (
-        <div className={styles.accardionWrapper}>
-            {modules?.sections.map((module: sectionT, index: number) => (
-                <AccardionItem key={module.section} openIndex={openIndex} moduleIndex={index} module={module}
-                               handleToggleOpen={handleToggleOpen}/>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className={styles.accardionWrapper}>
+      {modules?.sections.map((module: sectionT, index: number) => (
+        <AccardionItem key={module.section} openIndex={openIndex} moduleIndex={index} module={module} handleToggleOpen={handleToggleOpen} />
+      ))}
+    </div>
+  )
+})
