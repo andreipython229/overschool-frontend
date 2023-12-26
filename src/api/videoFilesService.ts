849b9e9d44bcf8dfc92092e1fa50ae0bab6b38d0
aleksejpetrovec <rbtechologics@mail.ru>
2023-12-26 11:27:10 +0300

    video fix;

--- src/api/videoFilesService.ts
+++ src/api/videoFilesService.ts
@@ -3,13 +3,13 @@ import { baseQuery } from './baseApi'
 
 export const videoFilesService = createApi({
   reducerPath: 'videoFilesService',
-  baseQuery: baseQuery(),
+  baseQuery: baseQuery('/video/'),
   tagTypes: ['lessons', 'videoFiles', 'patchLessons', 'uploadLessonVideo'],
   endpoints: build => ({
     uploadLessonVideo: build.mutation<void, { arg: { id: number; type: string; formdata: FormData }; schoolName: string }>({
       query: ({ arg, schoolName }) => {
         return {
-          url: `/${schoolName}/${arg.type}s/${arg.id}/`,
+          url: `/${schoolName}/${arg.type}_video/${arg.id}/`,
           method: 'PATCH',
           body: arg.formdata,
         }
