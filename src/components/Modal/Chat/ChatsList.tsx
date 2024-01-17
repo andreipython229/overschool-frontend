979b9df676de804701354479c6c49360452fa55a
Denis Sturchenko <developden@gmail.com>
2024-01-17 11:49:21 +0300

    added chat inform socket, added chat in studentsTables

--- src/components/Modal/Chat/ChatsList.tsx
+++ src/components/Modal/Chat/ChatsList.tsx
@@ -14,24 +14,26 @@ export const ChatsList: FC<chatsListT> = ({ chats }) => {
   const [sortedChats, setSortedChats] = useState(chats)
 
   useEffect(() => {
-    const chatsWithLastMessage = chats?.filter(chat => chat.last_message !== null);
-  const chatsWithoutLastMessage = chats?.filter(chat => chat.last_message === null);
+      if (chats) {
+          const chatsWithLastMessage = chats?.filter(chat => chat.last_message !== null);
+          const chatsWithoutLastMessage = chats?.filter(chat => chat.last_message === null);
 
-  // Сортируем чаты с последним сообщением по времени
-  const sortedChatsWithLastMessage = chatsWithLastMessage?.sort((a, b) => {
-    const dateA = new Date(a.last_message.sent_at).getTime();
-    const dateB = new Date(b.last_message.sent_at).getTime();
-    return dateB - dateA;
-  });
+          // Сортируем чаты с последним сообщением по времени
+          const sortedChatsWithLastMessage = chatsWithLastMessage?.sort((a, b) => {
+            const dateA = new Date(a.last_message.sent_at).getTime();
+            const dateB = new Date(b.last_message.sent_at).getTime();
+            return dateB - dateA;
+          });
 
-  // Объединяем чаты, сначала идут чаты с последним сообщением, затем без него
-  const mergedChats = sortedChatsWithLastMessage?.concat(chatsWithoutLastMessage ?? []);
-    setSortedChats(mergedChats)
-
+          // Объединяем чаты, сначала идут чаты с последним сообщением, затем без него
+          const mergedChats = sortedChatsWithLastMessage?.concat(chatsWithoutLastMessage ?? []);
+          setSortedChats(mergedChats)
+      }
   }, [chats])
 
   return (
     <div className={styles.chatsList}>
+      {/*<div>Chat count = {sortedChats?.length}</div>*/}
       {sortedChats?.map(chat => (
         <ChatPreview key={chat.id} chat={chat} />
       ))}
