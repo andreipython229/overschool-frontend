07422bbd51d6f6212bad17d18883060b3de44734
overschool2 <144776685+overschool2@users.noreply.github.com>
2024-01-17 16:05:43 +0300

    Правки по макету

--- src/Pages/Initial/InitPageHeader.tsx
+++ src/Pages/Initial/InitPageHeader.tsx
@@ -55,7 +55,10 @@ export const InitPageHeader: FC<InitPageHeaderPT> = memo(({ setLoginShow, setReg
 
   return (
     <header className={styles.init_header}>
-      <img src={logo} alt="Logotype ITOVERONE" />
+      <div className={styles.init_header_logo}>
+        <img src={logo} alt="Logotype ITOVERONE" />
+        <p> IT OVERONE</p>
+      </div>
       <div className={styles.btn_block}>
         {isLogin ? (
           <div className={styles.header_block}>
