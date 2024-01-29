3cd59c89c1f9233a7eee3bb9b30efc4fe68851f4
overschool2 <144776685+overschool2@users.noreply.github.com>
2024-01-29 13:51:04 +0300

    1.При созданиии круса обновляется страница с курсами
    2. Исправил навигационную панель

--- src/components/Navbar/MobileNavbar/MobileNavbar.tsx
+++ src/components/Navbar/MobileNavbar/MobileNavbar.tsx
@@ -20,6 +20,7 @@ import { ChatI, SenderI } from 'types/chatsT'
 import { setTotalUnread } from '../../../store/redux/chats/unreadSlice'
 import { setChats } from '../../../store/redux/chats/chatsSlice'
 import { isEqual, omit } from 'lodash'
+import { SvgIcon } from '@mui/material'
 
 import styles from '../navbar.module.scss'
 import { clearUserProfile } from 'store/redux/users/profileSlice'
@@ -28,6 +29,7 @@ import { removeHeaderId } from 'store/redux/school/headerIdSlice'
 import { removeSchoolName } from 'store/redux/school/schoolSlice'
 import { useCookies } from 'react-cookie'
 import { useFetchProfileDataQuery } from 'api/profileService'
+import { RoleE } from 'enum/roleE'
 
 interface IIsActive {
   isActive?: boolean
@@ -35,7 +37,7 @@ interface IIsActive {
 
 export const MobileNavbar: FC = memo(() => {
   const unRead = useSelector((state: RootState) => state.unread.totalUnread)
-  const { role } = useAppSelector(selectUser)
+  const { role: UserRole } = useAppSelector(selectUser)
   const isActive = ({ isActive }: IIsActive) => (isActive ? styles.isActive : '')
   const [logout] = useLazyLogoutQuery()
   const dispatch = useAppDispatch()
@@ -127,51 +129,74 @@ export const MobileNavbar: FC = memo(() => {
   return (
     <>
       <div className={styles.navbar_setting_account}>
-        {navlinkByRoles[role].map(({ path, icon }, index: number) => (
-          <Tooltip
-            title={
-              path === Path.Courses
-                ? 'Курсы'
-                : path === Path.CourseStats
-                ? 'Ученики школы'
-                : path === Path.HomeWork
-                ? 'Домашние задания'
-                : 'Настройки школы'
-            }
-            key={index}
-            arrow
-            placement={'right'}
-          >
-            <NavLink key={index} to={path} className={isActive}>
-              {icon}
-            </NavLink>
-          </Tooltip>
-        ))}
-        <Tooltip title={'Чаты'} arrow placement={'right'}>
-          <div className={`${styles.chatIcon} ${isChatOpen ? styles.chatIcon_active : ''}`} onClick={off}>
-            {/*<Badge badgeContent={unRead} color={Number(unRead) > 0 ? 'secondary' : "primary"}>*/}
-            {/*    <IconSvg width={38} height={34} viewBoxSize="0 0 28 24" path={chatIconPath}/>*/}
-            {/*</Badge>*/}
-            {Number(unRead) > 0 ? (
-              <Badge badgeContent={unRead} color="error">
-                <IconSvg width={38} height={34} viewBoxSize="0 0 28 24" path={chatIconPath} />
-              </Badge>
+          {navlinkByRoles[UserRole].map(({ path, icon }, index: number) =>
+            path !== 'doNotPath' ? (
+              <Tooltip
+                title={
+                  path === Path.Courses
+                    ? 'Курсы'
+                    : path === Path.CourseStats
+                    ? 'Ученики школы'
+                    : path === Path.HomeWork
+                    ? 'Домашние задания'
+                    : 'Настройки школы'
+                }
+                key={index}
+                arrow
+                placement={'right'}
+              >
+                <NavLink key={index} to={path} className={isActive}>
+                  {icon}
+                </NavLink>
+              </Tooltip>
             ) : (
-              <IconSvg width={38} height={34} viewBoxSize="0 0 28 24" path={chatIconPath} />
-            )}
-          </div>
-        </Tooltip>
+              <>
+                <Tooltip title={'Чаты'} arrow placement={'right'}>
+                  <div className={`${styles.chatIcon} ${isChatOpen ? styles.chatIcon_active : ''}`} onClick={off}>
+                    {Number(unRead) > 0 ? (
+                      <Badge badgeContent={unRead} color="error">
+                        <IconSvg width={38} height={34} viewBoxSize="0 0 28 24" path={chatIconPath} />
+                      </Badge>
+                    ) : (
+                      <IconSvg width={38} height={34} viewBoxSize="0 0 28 24" path={chatIconPath} />
+                    )}
+                  </div>
+                </Tooltip>
+              </>
+            ),
+          )}
 
-        <NavLink to={Path.Profile} className={isActive}>
-          <svg width="38" height="38" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M15 27.5C8.09625 27.5 2.5 21.9037 2.5 15C2.5 8.09625 8.09625 2.5 15 2.5C21.9037 2.5 27.5 8.09625 27.5 15C27.5 21.9037 21.9037 27.5 15 27.5ZM8.76625 22.82C10.5357 24.2346 12.7346 25.0036 15 25C17.4625 25 19.7162 24.11 21.4587 22.635C20.6447 21.7997 19.6713 21.1361 18.5963 20.6835C17.5213 20.2309 16.3664 19.9985 15.2 20C13.9907 19.9986 12.7944 20.2486 11.6868 20.734C10.5793 21.2195 9.58473 21.9298 8.76625 22.82ZM7.02 21.025C8.07057 19.9101 9.33834 19.0221 10.7452 18.4159C12.152 17.8097 13.6681 17.4979 15.2 17.5C16.6771 17.4981 18.14 17.788 19.5047 18.353C20.8694 18.918 22.1091 19.747 23.1525 20.7925C24.2226 19.2864 24.8545 17.5133 24.9782 15.6699C25.1019 13.8265 24.7124 11.9849 23.8531 10.3493C22.9938 8.71378 21.6982 7.34828 20.11 6.40431C18.5218 5.46033 16.7031 4.9748 14.8558 5.00157C13.0084 5.02833 11.2046 5.56636 9.6444 6.55595C8.08422 7.54554 6.8287 8.948 6.01711 10.6078C5.20553 12.2675 4.86962 14.1196 5.04664 15.9587C5.22367 17.7977 5.90672 19.5518 7.02 21.0262V21.025ZM15 16.25C13.6739 16.25 12.4021 15.7232 11.4645 14.7855C10.5268 13.8479 10 12.5761 10 11.25C10 9.92392 10.5268 8.65215 11.4645 7.71447C12.4021 6.77678 13.6739 6.25 15 6.25C16.3261 6.25 17.5979 6.77678 18.5355 7.71447C19.4732 8.65215 20 9.92392 20 11.25C20 12.5761 19.4732 13.8479 18.5355 14.7855C17.5979 15.7232 16.3261 16.25 15 16.25ZM15 13.75C15.663 13.75 16.2989 13.4866 16.7678 13.0178C17.2366 12.5489 17.5 11.913 17.5 11.25C17.5 10.587 17.2366 9.95107 16.7678 9.48223C16.2989 9.01339 15.663 8.75 15 8.75C14.337 8.75 13.7011 9.01339 13.2322 9.48223C12.7634 9.95107 12.5 10.587 12.5 11.25C12.5 11.913 12.7634 12.5489 13.2322 13.0178C13.7011 13.4866 14.337 13.75 15 13.75Z"
-              fill="#E0DCED"
-            />
-          </svg>
-        </NavLink>
-
-        <NavLink to={Path.InitialPage} title={'Выход из профиля'} onClick={logOut}>
+          {UserRole === RoleE.Admin && (
+            <Tooltip title={'Связаться с техподдержкой'} arrow placement={'right'}>
+              <a key={'techsupport'} href={'https://t.me/over_school'} target="_blank" rel="noreferrer">
+                <SvgIcon
+                  className={styles.navbar_menu}
+                  viewBox={'0 0 508 508'}
+                  xmlSpace="preserve"
+                  style={{ opacity: '0.8', fontSize: '3.5em', padding: '0.15em', marginBottom: '-0.5rem' }}
+                >
+                  <circle style={{ fill: '#54C0EB' }} cx="254" cy="254" r="254" />
+                  <path
+                    style={{ fill: '#FFFFFF' }}
+                    d="M303.7,303.3c30.5-17.3,51-50.1,51-87.6c0-55.7-45.1-100.8-100.8-100.8S153.2,160,153.2,215.6
+	c0,37.6,20.6,70.3,51,87.6C141,319.3,89.7,365,66,424.8c46.5,51.1,113.5,83.2,188,83.2s141.5-32.1,188-83.2
+	C418.3,365,367,319.3,303.7,303.3z"
+                  />
+                  <path
+                    style={{ fill: '#324A5E' }}
+                    d="M401.6,182.3h-15.8C370.9,123.4,317.5,79.6,254,79.6s-116.9,43.7-131.8,102.7h-15.8
+	c-5.4,0-9.8,4.4-9.8,9.8V240c0,5.4,4.4,9.8,9.8,9.8h20c6.1,0,10.8-5.5,9.7-11.4c-2-10.4-2.7-21.3-1.8-32.5
+	c4.8-59.3,53.6-106.9,113.1-110.1c69.2-3.8,126.8,51.5,126.8,119.9c0,7.8-0.8,15.3-2.2,22.7c-1.2,6,3.6,11.5,9.6,11.5h1.8
+	c-4.2,13-14.9,37.2-38.3,50.2c-19.6,10.9-44.3,11.9-73.4,2.8c-1.5-6.7-8.9-14.6-16.5-18.3c-9.8-4.9-15.9-0.8-19.4,6.2
+	s-3,14.3,6.7,19.2c8.6,4.3,21.6,5.2,27,0.5c13.9,4.3,26.9,6.5,39,6.5c15,0,28.5-3.3,40.4-10c27.5-15.3,38.8-43.7,42.8-57.2h9.9
+	c5.4,0,9.8-4.4,9.8-9.8v-47.9C411.4,186.7,407,182.3,401.6,182.3z"
+                  />
+                </SvgIcon>
+              </a>
+            </Tooltip>
+            
+          )}
+          <NavLink to={Path.InitialPage} title={'Выход из профиля'} onClick={logOut}>
           <div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -190,7 +215,7 @@ export const MobileNavbar: FC = memo(() => {
             </svg>
           </div>
         </NavLink>
-      </div>
+        </div>
       {isChatOpen && (
         <Portal closeModal={on}>
           <Chat closeModal={on} />
