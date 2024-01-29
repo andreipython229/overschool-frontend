163c3eddc1e55a9a421909fc4302917f7ba111f6
Dmitry Kulaga <dima.kulaga5@gmail.com>
2024-01-29 20:58:40 +0300

    Маленький фикс отображения ответа от бота

--- src/components/ChatGPT/index.tsx
+++ src/components/ChatGPT/index.tsx
@@ -98,7 +98,8 @@ const formatBotAnswer = (answer: string): JSX.Element => {
                 .replace(/</g, '&lt;')
                 .replace(/>/g, '&gt;')
                 .replace(/"/g, '&quot;')
-                .replace(/'/g, '&#039;');
+                .replace(/'/g, '&#039;')
+                .replace(/####/g, '');
   };
   const codeBlockRegex = /```([\s\S]*?)```/g;
   const replaceCodeBlocks = (_: any, code: string) => {
