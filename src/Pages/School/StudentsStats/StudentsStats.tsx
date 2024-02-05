b96c3cb47fc7b0230ac1b6bb55a5800b49d11cd7
overschool2 <144776685+overschool2@users.noreply.github.com>
2024-02-05 12:54:27 +0300

    В превью урока убрано название во втором (два раза фигурирует название ) Настройки группы, кнопки "Создать группу", "Добавить менторов"

--- src/Pages/School/StudentsStats/StudentsStats.tsx
+++ src/Pages/School/StudentsStats/StudentsStats.tsx
@@ -11,16 +11,20 @@ import { studentsGroupsT } from '../../../types/studentsGroup'
 import { ToggleButtonDropDown } from 'components/common/ToggleButtonDropDown'
 import { useBoolean } from '../../../customHooks'
 import { Portal } from '../../../components/Modal/Portal'
+import { headerUserRoleName } from '../../../config/headerUserRoleName'
 
 import styles from './studentsStats.module.scss'
 import { tableBallsStarPath } from 'config/commonSvgIconsPath'
 import { Path } from 'enum/pathE'
 import { useAppSelector } from 'store/hooks'
 import { schoolNameSelector } from 'selectors'
+import {selectUser} from '../../../selectors'
 
 export const StudentsStats = () => {
   const { course_id: courseId } = useParams()
 
+  const {role} = useAppSelector(selectUser)
+  
   const [hideStats, setHideStats] = useState<boolean>(true)
 
   const navigate = useNavigate()
@@ -50,6 +54,7 @@ export const StudentsStats = () => {
       <section className={styles.students_group}>
         <div className={styles.students_group_header}>
           <p className={styles.students_group_header_title}>Группы учеников</p>
+          {(headerUserRoleName[role] === 'Администратор') && (
           <div style={{display: 'flex'}}>
             <div onClick={offAddGroupModal} className={styles.students_group_header_add_group_btn}>
               <IconSvg width={22} height={18} viewBoxSize="0 0 22 18" path={createGroupIconPath} />
@@ -59,7 +64,7 @@ export const StudentsStats = () => {
               <IconSvg width={22} height={18} viewBoxSize="0 0 22 18" path={tableBallsStarPath} />
               Добавить менторов в школу
             </div>
-          </div>
+          </div>)}
         </div>
         <div className={styles.students_group_content_wrapper}>
           {dataToRender?.map(({ name, students, group_id }: studentsGroupsT) => {
