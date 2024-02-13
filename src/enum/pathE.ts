bb8fd459c8206a745330fd53cb540f1d33346aa8
aleksejpetrovec <rbtechologics@mail.ru>
2024-02-13 10:28:34 +0300

    Certificate completed task; fixed ChooseSchoolBug;

--- src/enum/pathE.ts
+++ src/enum/pathE.ts
@@ -23,7 +23,7 @@ export enum Path {
   CourseStudent = 'courses-stats/:course_id/students',
   CreateSchool = '/create-school/',
   Chat = '',
-  Certificate = '/certificate/:student_id/:course_id/:school_id/',
+  Certificate = '/certificate/:certLink',
 }
 
 export enum SettingsPath {
