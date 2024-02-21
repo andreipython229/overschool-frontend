3c2ec2425224c6c2bdbdbf5412eec41eb62b8ac8
ange2010 <112875401+ange2010@users.noreply.github.com>
2024-02-21 22:39:27 +0300

    fix excel export

--- src/components/StudentsTable/StudentsExport/StudentCourseExport.tsx
+++ src/components/StudentsTable/StudentsExport/StudentCourseExport.tsx
@@ -1,37 +1,24 @@
-import { FC, useEffect, useMemo, useState } from 'react'
+import { FC } from 'react'
 import { useParams } from 'react-router-dom'
 import { useLazyFetchAllCourseStatQuery } from 'api/courseStat'
-import { useAppDispatch, useAppSelector } from 'store/hooks'
-import { addFilters, removeFilter } from 'store/redux/filters/slice'
+import { useAppSelector } from 'store/hooks'
 import { Button } from '../../common/Button/Button'
-import {useDebounceFunc} from "../../../customHooks";
-import { IconSvg } from '../../common/IconSvg/IconSvg'
-import { downLoadIconPath } from '../../StudentsTableWrapper/config/svgIconsPath'
 import styles from '../../../Pages/School/StudentsStats/studentsStats.module.scss'
 import * as XLSX from "xlsx"
 
 export const StudentsCourseExport: FC = () => {
   const { course_id } = useParams()
   const schoolName = window.location.href.split('/')[4]
-  const dispatch = useAppDispatch()
   const filters = useAppSelector(state => state.filters['studentsPerCourse'])
-  const debounce = useDebounceFunc(dispatch)
-  const [fetchStudents, { data, isFetching }] = useLazyFetchAllCourseStatQuery()
+  const [fetchStudents] = useLazyFetchAllCourseStatQuery()
 
-  const handleReloadTable = () => {
-    fetchStudents({ id: String(course_id), filters, schoolName })
-  }
-
-  useEffect(() => {
-    handleReloadTable()
-  }, [filters])
-
-  const handleOnExport = () => {
-        const allStudents = data ?? [];
-        const wb = XLSX.utils.book_new(),
-        ws = XLSX.utils.json_to_sheet(allStudents);
-        XLSX.utils.book_append_sheet(wb, ws, "MySheet1");
-        XLSX.writeFile(wb, "Ученики курса.xlsx");
+  const handleOnExport = async () => {
+     const response = await fetchStudents({ id: String(course_id), filters, schoolName });
+     const allStudents = response.data ?? [];
+     const wb = XLSX.utils.book_new(),
+     ws = XLSX.utils.json_to_sheet(allStudents);
+     XLSX.utils.book_append_sheet(wb, ws, "MySheet1");
+     XLSX.writeFile(wb, "Ученики курса.xlsx");
   };
 
    return (
--- src/components/StudentsTable/StudentsExport/StudentsCroupExport.tsx
+++ src/components/StudentsTable/StudentsExport/StudentsCroupExport.tsx
@@ -1,36 +1,24 @@
-import { FC, useEffect, useMemo, useState } from 'react'
+import { FC } from 'react'
 import { useParams } from 'react-router-dom'
 import { useLazyFetchAllStudentsPerGroupQuery } from 'api/courseStat'
-import { useAppDispatch, useAppSelector } from 'store/hooks'
+import { useAppSelector } from 'store/hooks'
 import { Button } from '../../common/Button/Button'
-import {useDebounceFunc} from "../../../customHooks";
-import { IconSvg } from '../../common/IconSvg/IconSvg'
-import { downLoadIconPath } from '../../StudentsTableWrapper/config/svgIconsPath'
 import styles from '../../../Pages/School/StudentsStats/studentsStats.module.scss'
 import * as XLSX from "xlsx"
 
 export const StudentsCroupExport: FC = () => {
     const { group_id } = useParams()
-    const dispatch = useAppDispatch()
     const schoolName = window.location.href.split('/')[4]
     const filters = useAppSelector(state => state.filters['studentsPerGroup'])
-    const debounce = useDebounceFunc(dispatch)
-    const [fetchStudents, { data, isFetching }] = useLazyFetchAllStudentsPerGroupQuery()
+    const [fetchStudents] = useLazyFetchAllStudentsPerGroupQuery()
 
-    const handleReloadTable = () => {
-       fetchStudents({ id: group_id, filters, schoolName })
-    }
-
-    useEffect(() => {
-       handleReloadTable()
-    }, [filters])
-
-  const handleExport = () => {
-      const allStudents = data ?? [];
-      const wb = XLSX.utils.book_new(),
-      ws = XLSX.utils.json_to_sheet(allStudents);
-      XLSX.utils.book_append_sheet(wb, ws, "MySheet1");
-      XLSX.writeFile(wb, "Ученики группы.xlsx");
+ const handleExport = async () => {
+    const response = await fetchStudents({ id: group_id, filters, schoolName });
+    const allStudents = response.data ?? [];
+    const wb = XLSX.utils.book_new(),
+    ws = XLSX.utils.json_to_sheet(allStudents);
+    XLSX.utils.book_append_sheet(wb, ws, "MySheet1");
+    XLSX.writeFile(wb, "Ученики группы.xlsx");
   };
 
   return (
