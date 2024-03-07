2cd87eacaa0e56f7ea9d8c790c60729db1ad3a8e
aleksejpetrovec <rbtechologics@mail.ru>
2024-03-07 20:49:09 +0300

    initial page stylesheet fixes;

--- src/Pages/Initial/CourseImg.tsx
+++ src/Pages/Initial/CourseImg.tsx
@@ -10,6 +10,7 @@ export const CourseImg: FC<CourseImgPropsT> = memo(({ currentCourse, changeCurre
         className={currentCourse === id ? styles.init_main_course_block : styles.init_main_course_block + ' ' + styles.smallImg}
         style={style}
         onMouseEnter={() => changeCurrentCourse(id)}
+        onMouseLeave={() => changeCurrentCourse('-1')}
       >
         <h2 className={currentCourse === id ? styles.init_main_course_block_title : styles.init_main_course_block_title + ' ' + styles.title_rotate}>
           <strong>{title}</strong>
