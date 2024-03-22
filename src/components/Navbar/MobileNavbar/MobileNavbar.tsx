cd90c09508a875f62422d70407d4e453c6dd5ade
overschool2 <144776685+overschool2@users.noreply.github.com>
2024-03-22 14:58:16 +0300

    Баги в мобильной верстке

--- src/components/Navbar/MobileNavbar/MobileNavbar.tsx
+++ src/components/Navbar/MobileNavbar/MobileNavbar.tsx
@@ -226,8 +226,8 @@ export const MobileNavbar: FC = memo(() => {
                 <div className={styles.tariffPlan} style={{ textDecoration: 'none' }} onClick={handleClick}>
                 <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
+              width="36"
+              height="36"
               viewBox="0 0 24 24"
               fill="none"
               stroke="#e0dced"
