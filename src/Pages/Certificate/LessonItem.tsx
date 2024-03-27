f306a8fe0043104f6aa5a48c1b6562994109ceaa
overschool2 <144776685+overschool2@users.noreply.github.com>
2024-03-27 13:52:48 +0300

    Сертификат

---
+++ src/Pages/Certificate/LessonItem.tsx
@@ -0,0 +1,20 @@
+import { FC, memo } from 'react'
+import { lessonSvgMapper } from 'config/index'
+
+import styles from '../../components/StudentAccardion/ExerciseItem/exerciseItem.module.scss'
+import { Lesson } from 'types/courseStatT'
+
+type lessonItemT = {
+  lesson: Lesson
+}
+
+export const LessonItem: FC<lessonItemT> = memo(({ lesson }) => {
+  return (
+    <div className={styles.accardionWrapper_component_exerciseWrapper_exercise}>
+      {lessonSvgMapper[lesson.type]}
+      <div className={styles.accardionWrapper_component_exerciseWrapper_exercise_nameWrapper}>
+        <h5 className={styles.accardionWrapper_component_exerciseWrapper_exercise_nameWrapper_title}>{lesson.name}</h5>
+      </div>
+    </div>
+  )
+})
