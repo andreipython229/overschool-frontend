2a5d86bbac5ca51e16d80bfb406f283ec0b06c2b
Dima <dima.scherbakov20000@gmail.com>
2024-04-09 12:55:58 +0300

    Обновленный мануал работы с OVERAI

--- src/Pages/HelpCenter/HelpOverAI/index.tsx
+++ src/Pages/HelpCenter/HelpOverAI/index.tsx
@@ -5,7 +5,10 @@ import { logo } from "../../../assets/img/common";
 import { Button } from "../../../components/common/Button/Button";
 import overai_button from "../../../assets/img/overAI/overai_button.png";
 import overai_chat from "../../../assets/img/overAI/overai_chat.png";
-import overai_second_chat from '../../../assets/img/overAI/overai_second_chat.png';
+import overai_example_code from '../../../assets/img/overAI/overai_example_code.png';
+import overai_example_learning from '../../../assets/img/overAI/overai_example_learning.png';
+import overai_example_automation from '../../../assets/img/overAI/overai_example_automation.png';
+import overai_example_algorithm from '../../../assets/img/overAI/overai_example_algorithm.png';
 import firstStep from '../../../assets/img/createProject/firstStep.png'
 import secondStep from '../../../assets/img/createProject/secondStep.png'
 
@@ -50,7 +53,7 @@ export const HelpOverAI = () => {
             <div className={styles.HelpCenterPage_FAQ}>
                 <h1>Работа с помощником OVERAI</h1>
                 <h2>Как взаимодействовать с умным помощником на платформе</h2>
-                <p>После авторизации на платформе вам будет доступен умный помощник OVERAI.<br />Он будет находиться на каждой странице в <b>правом нижнем углу экрана.</b> 
+                <p>После авторизации на платформе вам будет доступен умный помощник OVERAI.<br />Он будет находиться на каждой странице в <b>правом нижнем углу экрана.</b>
                 </p>
                 <div className={styles.HelpCenterPage_FAQ_images}>
                     <img src={overai_button} alt="кнопка OverAI" className={styles.HelpCenterPage_FAQ_images_firstStep} />
@@ -64,23 +67,37 @@ export const HelpOverAI = () => {
                 </div>
                 <h2>Возможности умного помощника</h2>
                 <p>
-                OVERAI очень полезен благодаря своим возможностям генерации и анализа кода. Вот некоторые примеры того, как его можно использовать:
+                    OVERAI очень полезен благодаря своим возможностям генерации и анализа кода. Вот некоторые примеры того, как его можно использовать:
                 </p>
                 <p>
                     <b>Генерация кода</b>: OVERAI может помочь в написании фрагментов кода или даже целых программ на различных языках программирования.
-                    <br />
+                    
+                </p>
+                <div className={styles.HelpCenterPage_FAQ_images}>
+                    <img src={overai_example_code} alt="пример взаимодействия с помощником" className={styles.HelpCenterPage_FAQ_images_addStudents} />
+                </div>
+                <p>
                     <b>Обучение и поддержка</b>: OVERAI может служить инструментом для обучения новичков, объясняя синтаксис и основные концепции языков программирования.
-                    <br />
+                </p>
+                <div className={styles.HelpCenterPage_FAQ_images}>
+                    <img src={overai_example_learning} alt="пример взаимодействия с помощником" className={styles.HelpCenterPage_FAQ_images_addStudents} />
+                </div>
+                <p>
                     <b>Автоматизация задач</b>: OVERAI может автоматизировать рутинные задачи, такие как написание шаблонного кода или юнит-тестов.
-                    <br />
+                </p>
+                <div className={styles.HelpCenterPage_FAQ_images}>
+                    <img src={overai_example_automation} alt="пример взаимодействия с помощником" className={styles.HelpCenterPage_FAQ_images_addStudents} />
+                </div>
+                <p>
                     <b>Решение алгоритмических задач</b>: OVERAI может помочь найти решения для задач на алгоритмы и предоставить подробные объяснения.
                 </p>
-                    
-                
-                
                 <div className={styles.HelpCenterPage_FAQ_images}>
-                    <img src={overai_second_chat} alt="пример взаимодействия с помощником" className={styles.HelpCenterPage_FAQ_images_addStudents} />
+                    <img src={overai_example_algorithm} alt="пример взаимодействия с помощником" className={styles.HelpCenterPage_FAQ_images_addStudents} />
                 </div>
+
+
+
+
                 <p>Стоит понимать что OVERAI может помочь вам в любой ситуации.
                     <br />
                     Однако, советуем вам самостоятельно практиковать материалы в курсах. А когда понадобиться помощь, то вы всегда можете обратиться к умному помощнику
@@ -97,6 +114,6 @@ export const HelpOverAI = () => {
                     <img src={secondStep} alt="Создать проект" className={styles.HelpCenterPage_banner_images_secondStep} />
                 </div>
             </div>
-        </section>
+        </section >
     )
 }
