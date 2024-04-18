211bc88846b7457e3ef11fccdf64f7e236bfb20c
aleksejpetrovec <rbtechologics@mail.ru>
2024-04-18 10:15:01 +0300

    changes for sandbox;

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api/socket.io',
     createProxyMiddleware({
-      target: 'https://apidev.overschool.by',
+      target: 'http://sanbox.overschool.by',
       ws: true,
       changeOrigin: true,
       secure: false,
@@ -14,7 +14,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://apidev.overschool.by',
+      target: 'http://sanbox.overschool.by',
       changeOrigin: true,
       secure: false,
     }),
@@ -22,11 +22,11 @@ module.exports = function (app) {
   app.use(
     '/video',
     createProxyMiddleware({
-      target: 'http://45.135.234.137:8000',
+      target: 'http://45.87.219.3:8000',
       changeOrigin: false,
       secure: false,
       onProxyReq(proxyReq, req, res) {
-        proxyReq.setHeader('origin', 'http://45.135.234.137:8000')
+        proxyReq.setHeader('origin', 'http://45.87.219.3:8000')
       },
     }),
   )
