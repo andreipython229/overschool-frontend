3826dd3bcf0f56504f7419acbd1e30474f6092da
aleksejpetrovec <rbtechologics@mail.ru>
2024-04-18 15:18:56 +0300

    fix;

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api/socket.io',
     createProxyMiddleware({
-      target: 'http://sanbox.overschool.by',
+      target: 'http://sandbox.overschool.by',
       ws: true,
       changeOrigin: true,
       secure: false,
@@ -14,7 +14,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://sanbox.overschool.by',
+      target: 'http://sandbox.overschool.by',
       changeOrigin: true,
       secure: false,
     }),
