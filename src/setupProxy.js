bd7123cd7a7e7596c55e56db686f291074c6fc69
aleksejpetrovec <rbtechologics@mail.ru>
2024-06-04 08:59:27 +0300

    production merge;

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,8 +6,8 @@ module.exports = function (app) {
   app.use(
     '/api/socket.io',
     createProxyMiddleware({
-      target: 'http://sandbox.overschool.by',
-      // target: 'https://apidev.overschool.by',
+      // target: 'http://sandbox.overschool.by',
+      target: 'https://apidev.overschool.by',
       ws: true,
       changeOrigin: true,
       secure: false,
@@ -17,8 +17,8 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      // target: 'https://apidev.overschool.by',
-      target: 'http://sandbox.overschool.by',
+      target: 'https://apidev.overschool.by',
+      // target: 'http://sandbox.overschool.by',
       changeOrigin: true,
       secure: false,
     }),
@@ -26,13 +26,13 @@ module.exports = function (app) {
   app.use(
     '/video',
     createProxyMiddleware({
-      // target: 'http://45.135.234.137:8000',
-      target: 'http://45.87.219.3:8000',
+      target: 'http://45.135.234.137:8000',
+      // target: 'http://45.87.219.3:8000',
       changeOrigin: false,
       secure: false,
       onProxyReq(proxyReq, req, res) {
-        // proxyReq.setHeader('origin', 'http://45.135.234.137:8000')
-        proxyReq.setHeader('origin', 'http://45.87.219.3:8000')
+        proxyReq.setHeader('origin', 'http://45.135.234.137:8000')
+        // proxyReq.setHeader('origin', 'http://45.87.219.3:8000')
       },
     }),
   )
