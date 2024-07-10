19dc2bd582a4bb5fb9c89070b3e7783ce9eb09bf
Dmitry Kulaga <dima.kulaga5@gmail.com>
2024-07-10 13:14:07 +0300

    Выбор нескольих вариантов ответа в тестах

--- src/Pages/StudentCourse/StudentTestBlock/StudentAnswer/index.tsx
+++ src/Pages/StudentCourse/StudentTestBlock/StudentAnswer/index.tsx
@@ -1,22 +1,39 @@
-import {FC, memo} from 'react';
-import {Radio} from 'components/common/Radio/Radio';
+import { FC, useState, useEffect, useRef } from 'react';
+import { Checkbox } from 'components/common/Checkbox/Checkbox'; // Измените на ваш компонент чекбокса
 import styles from './studentAnswer.module.scss';
-import {AnswersT} from "../../../../components/AddQuestion";
+import { AnswersT } from '../../../../components/AddQuestion';
 
 type StudentAnswerProps = {
     id: string;
     title: string;
     name: string;
-    onSelect: (answerCorrect: boolean, answewr_id: string, title: string) => void;
+    onSelect: (answerCorrect: boolean, answer_id: string, title: string) => void;
     isCorrect: boolean;
     picture?: string;
+    resetSelection: boolean; // Добавляем проп для сброса выбора
 };
 
-export const StudentAnswer: FC<StudentAnswerProps> = ({id, title, name, onSelect, isCorrect, picture}) => {
+export const StudentAnswer: FC<StudentAnswerProps> = ({ id, title, name, onSelect, isCorrect, picture, resetSelection }) => {
+    const [selected, setSelected] = useState<boolean>(false); // Состояние для отслеживания выбранности ответа
+    const prevResetSelection = useRef<boolean>(false); // Ссылка на предыдущее значение resetSelection
+
+    useEffect(() => {
+        if (prevResetSelection.current !== resetSelection) {
+            setSelected(false); // Сбрасываем выбор при изменении resetSelection
+            prevResetSelection.current = resetSelection; // Обновляем предыдущее значение
+        }
+    }, [resetSelection]);
+
+    const handleSelect = () => {
+        setSelected(!selected); // Инвертируем состояние выбранности
+        onSelect(isCorrect, id, title); // Вызываем колбэк onSelect с информацией о выборе
+    };
+
     return (
-        <div className={styles.wrapper} onClick={() => onSelect(isCorrect, id, title)}>
+        <div className={styles.wrapper} onClick={handleSelect}>
             <div className={styles.answer}>
-                <Radio title={title} id={id} name={name} key={`${id}`} />
+                <Checkbox checked={selected} /> {/* Используйте компонент чекбокса или другого типа элемента для отображения выбора */}
+                <span className={styles.answerTitle}>{title}</span>
             </div>
             {picture && (
                 <img src={picture} alt={title} className={styles.image} />
--- src/Pages/StudentCourse/StudentTestBlock/StudentQuestion/index.tsx
+++ src/Pages/StudentCourse/StudentTestBlock/StudentQuestion/index.tsx
@@ -30,59 +30,85 @@ type StudentQuestionT = {
 }
 
 export const StudentQuestion: FC<StudentQuestionT> = ({
-                                                          questions,
-                                                          length,
-                                                          numberTest,
-                                                          setNumberTest,
-                                                          userAnswers,
-                                                          updateUserAnswers,
-                                                          onCompleteTest,
-                                                          userAnswerFull,
-                                                          setUserAnswerFull,
-                                                          test,
-                                                          user,
-                                                          setShowResult,
-                                                          setUserPercent
-                                                      }) => {
-    const questionLength = length.length
-    const schoolName = window.location.href.split('/')[4]
+    questions,
+    length,
+    numberTest,
+    setNumberTest,
+    userAnswers = {},
+    updateUserAnswers,
+    onCompleteTest,
+    userAnswerFull,
+    setUserAnswerFull,
+    test,
+    user,
+    setShowResult,
+    setUserPercent
+}) => {
+    const questionLength = length.length;
+    const schoolName = window.location.href.split('/')[4];
     const nameAnswer = (questions && questions.body.length > 0) ? questions.body : '';
-    const progress = (100 / questionLength) * (numberTest + 1)
-    const [buttonDisabled, setButtonDisabled] = useState<boolean>(true)
-    const [sendTestResults, {isSuccess}] = useSendTestResultsMutation();
+    const progress = (100 / questionLength) * (numberTest + 1);
+    const [buttonDisabled, setButtonDisabled] = useState<boolean>(true);
+    const [resetSelection, setResetSelection] = useState<boolean>(false);
+    const [sendTestResults, { isSuccess }] = useSendTestResultsMutation();
 
     const handleAnswerSelect = (isCorrect: boolean, answer_id: string, title: string) => {
-        updateUserAnswers((prevAnswers: AnswersType) => ({
-            ...prevAnswers,
-            [questions.question_id]: isCorrect,
-        }));
-        const answer: AnswersT = {
-            answer_id: parseInt(answer_id),
+        const answerIdNum = parseInt(answer_id); // Преобразование answer_id в число
+        const selectedAnswer = {
+            answer_id: answerIdNum,
             body: title,
             is_correct: isCorrect,
             question: questions.question_id
         };
 
-        setUserAnswerFull((prevUserAnswer: AnswersT) => ({
-            ...prevUserAnswer,
-            [questions.question_id]: answer,
-        }));
-        setButtonDisabled(false)
+        updateUserAnswers((prevAnswers: AnswersType) => {
+            const currentAnswers = Array.isArray(prevAnswers[questions.question_id]) ? prevAnswers[questions.question_id] : [] as number[];
+            if (!Array.isArray(currentAnswers)) {
+                console.error("Expected currentAnswers to be an array.");
+                return prevAnswers;
+            }
+            const updatedAnswers = currentAnswers.includes(answerIdNum)
+                ? currentAnswers.filter((id: number) => id !== answerIdNum)
+                : [...currentAnswers, answerIdNum];
+            return {
+                ...prevAnswers,
+                [questions.question_id]: updatedAnswers,
+            };
+        });
+
+        setUserAnswerFull((prevUserAnswer: AnswersType) => {
+            const currentAnswers = Array.isArray(prevUserAnswer[questions.question_id]) ? prevUserAnswer[questions.question_id] : [] as any[];
+            if (!Array.isArray(currentAnswers)) {
+                console.error("Expected currentAnswers to be an array.");
+                return prevUserAnswer;
+            }
+            const updatedAnswers = currentAnswers.some((ans: any) => ans.answer_id === selectedAnswer.answer_id)
+                ? currentAnswers.filter((ans: any) => ans.answer_id !== selectedAnswer.answer_id)
+                : [...currentAnswers, selectedAnswer];
+            return {
+                ...prevUserAnswer,
+                [questions.question_id]: updatedAnswers,
+            };
+        });
+
+        setButtonDisabled(false);
     }
 
     const handleNextQ = () => {
         if (numberTest + 1 <= questionLength) {
             setNumberTest(numberTest + 1);
+            setResetSelection(true);
         }
-        setButtonDisabled(true)
+        setButtonDisabled(true);
     }
 
     const percentage = () => {
         const totalQuestions = Object.keys(userAnswers).length;
-        const correctAnswers = Object.values(userAnswers).filter(answer => answer).length;
+        const correctAnswers = Object.values(userAnswers).filter(answerArray =>
+            Array.isArray(answerArray) && answerArray.every((answer: any) => answer.is_correct)).length;
         return (
             parseFloat((correctAnswers / totalQuestions * 100).toFixed(2))
-        )
+        );
     }
 
     const handleCompleteTest = () => {
@@ -93,10 +119,10 @@ export const StudentQuestion: FC<StudentQuestionT> = ({
             test: test as number,
             user: user
         }
-        sendTestResults({body: testResults, schoolName}).unwrap().then(() => {
-            setShowResult(true)
+        sendTestResults({ body: testResults, schoolName }).unwrap().then(() => {
+            setShowResult(true);
         }).catch((error: { data: any }) => {
-            console.log(error.data)
+            console.log(error.data);
         });
     }
 
@@ -111,31 +137,29 @@ export const StudentQuestion: FC<StudentQuestionT> = ({
             <p className={styles.wrapper_question}>{questions?.body}</p>
             {questions?.picture ? (
                 <div style={{ marginBottom: '15px', alignContent: 'center' }}>
-                    <img src={questions?.picture} alt="Question Image" width={300} height={275} style={{ borderRadius: '10px', display: 'block' }}/>
+                    <img src={questions?.picture} alt="Question Image" width={300} height={275} style={{ borderRadius: '10px', display: 'block' }} />
                 </div>
             ) : (
                 ''
-            )
-
-            }
+            )}
             <div className={styles.wrapper_progressBar}>
-                <div className={styles.wrapper_progressBar_progress} style={{width: `${progress}%`}}></div>
+                <div className={styles.wrapper_progressBar_progress} style={{ width: `${progress}%` }}></div>
             </div>
             {questions?.answers &&
-                questions?.answers.map(({body: answer, answer_id: id, is_correct: isCorrect, picture}: any, index: number) => (
-                    <StudentAnswer key={index} id={id} title={answer} name={nameAnswer} isCorrect={isCorrect} picture={picture}
-                                   onSelect={handleAnswerSelect}/>
+                questions?.answers.map(({ body: answer, answer_id: id, is_correct: isCorrect, picture }: any, index: number) => (
+                    <StudentAnswer key={index} id={id} title={answer} name={nameAnswer} isCorrect={isCorrect} picture={picture} resetSelection={resetSelection}
+                        onSelect={handleAnswerSelect} />
                 ))}
             {numberTest + 1 !== questionLength ?
                 (questions?.question_id !== -9999 ?
-                        <span className={styles.wrapper_button}>
-                            <Button disabled={buttonDisabled} onClick={handleNextQ} text={'Следующий вопрос'}
-                                    variant="primary"/></span>
-                        :
-                        <></>
+                    <span className={styles.wrapper_button}>
+                        <Button disabled={buttonDisabled} onClick={handleNextQ} text={'Следующий вопрос'}
+                            variant="primary" /></span>
+                    :
+                    <></>
                 ) : <span className={styles.wrapper_button}>
                     <Button onClick={handleCompleteTest} text={'Завершить тест'} disabled={buttonDisabled}
-                            variant="primary"/></span>
+                        variant="primary" /></span>
             }
         </div>
     )
