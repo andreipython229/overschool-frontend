c991874fde4a34841464846821701a8eacde0b36
overschool2 <144776685+overschool2@users.noreply.github.com>
2024-07-19 11:16:22 +0300

    Правки верстки

--- src/Pages/School/Navigations/CoursesCreating/CoursePage.tsx
+++ src/Pages/School/Navigations/CoursesCreating/CoursePage.tsx
@@ -129,7 +129,7 @@ export const CoursePage: FC = () => {
     <>
       <div className={styles.container}>
         <AnimatePresence>
-          <div style={{ display: 'flex', gap: '1rem', paddingBottom: '1rem', width:'85%', margin: '0 auto' }}>
+          <div style={{ display: 'flex', gap: '1rem', paddingBottom: '1rem' }}>
             {activeFolder.length > 0 && (
               <Chip
                 label={'Убрать фильтрацию'}
