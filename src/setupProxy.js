97d8d72e079e0a7cfa6f6695c093f0895035c59d
aleksejpetrovec <rbtechologics@mail.ru>
2024-08-18 12:20:41 +0300

    after merge commit;

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,8 +6,8 @@ module.exports = function (app) {
   app.use(
     '/api/socket.io',
     createProxyMiddleware({
-      // target: 'http://sandbox.coursehb.ru',
-      target: 'https://apidev.coursehb.ru',
+      target: 'http://sandbox.coursehb.ru',
+      // target: 'https://apidev.coursehb.ru',
       ws: true,
       changeOrigin: true,
       secure: false,
@@ -17,8 +17,8 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      // target: 'http://sandbox.coursehb.ru',
-      target: 'https://apidev.coursehb.ru',
+      target: 'http://sandbox.coursehb.ru',
+      // target: 'https://apidev.coursehb.ru',
       changeOrigin: true,
       secure: false,
     }),
@@ -26,13 +26,13 @@ module.exports = function (app) {
   app.use(
     '/video',
     createProxyMiddleware({
-      // target: 'http://45.135.234.21:8000',
-      target: 'http://45.135.234.9:8000',
+      target: 'http://45.135.234.21:8000',
+      // target: 'http://45.135.234.9:8000',
       changeOrigin: false,
       secure: false,
       onProxyReq(proxyReq, req, res) {
-        // proxyReq.setHeader('origin', 'http://45.135.234.21:8000')
-        proxyReq.setHeader('origin', 'http://45.135.234.9:8000')
+        proxyReq.setHeader('origin', 'http://45.135.234.21:8000')
+        // proxyReq.setHeader('origin', 'http://45.135.234.9:8000')
       },
     }),
   )
