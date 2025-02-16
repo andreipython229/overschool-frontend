4c4d63f34a403d16acab71ae09ce4233e04162f0
Na1x-dev <na1x.1024@gmail.com>
2025-02-16 15:20:02 +0300

    feat: rebuilded help/groups

--- src/Pages/HelpCenter/HelpGroupSettings/index.tsx
+++ src/Pages/HelpCenter/HelpGroupSettings/index.tsx
@@ -1,99 +1,224 @@
-import { Button } from '../../../components/common/Button/Button'
-import groups from '../../../assets/img/manageSchool/groups.png'
-import groups1 from '../../../assets/img/manageSchool/groups1.png'
-import groups2 from '../../../assets/img/manageSchool/groups2.png'
-import students4 from '../../../assets/img/createProject/students4.png'
-import students5 from '../../../assets/img/createProject/students5.png'
-import students6 from '../../../assets/img/createProject/students6.png'
+
+
+// export const HelpGroupSettings = () => {
+//       const navigate = useNavigate()
+
+//       const handleHelpPage = () => {
+//         navigate(generatePath(Path.HelpPage))
+//       }
+
+//       const handleLoginPage = () => {
+//           navigate(generatePath(Path.LoginPage))
+//       }
+
+//       const handleRegistrationUser = () => {
+//           navigate(generatePath(Path.CreateSchool))
+//       }
+
+//       return (
+//         <section className={styles.HelpCenterPage}>
+//           <div className={styles.init_header}>
+//                <a
+//                   href={Path.InitialPage}
+//                   className={styles.init_header_logo}
+//                   style={{
+//                      textDecoration: 'none',
+//                      color: '#ba75ff',
+//                      fontWeight: 'bold',
+//                      padding: '0.5rem',
+//                      borderRadius: '10px',
+//                   }}
+//                 >
+//                    <img src={logo} alt="Logotype ITOVERONE" />
+//                    <p> IT OVERONE</p>
+//                 </a>
+//                 <div className={styles.header_block}>
+//                    <Button onClick={handleHelpPage} variant={'logIn'} style={{ fontSize: '18px', fontWeight: '700' }} text={'Помощь'} />
+//                    <Button onClick={handleLoginPage} variant={'logIn'} style={{ fontSize: '18px', fontWeight: '700' }} text={'Войти'} />
+//                    <Button onClick={handleRegistrationUser} variant={'logIn'} style={{ fontSize: '18px', fontWeight: '700' }} text={'Создать платформу'} />
+//                 </div>
+//           </div>
+//           <div className={styles.HelpCenterPage_FAQ}>
+//             <h1> Настройки группы </h1>
+//             <h2> Переходим к настройкам группы</h2>
+//             <p> 
+//               Для того, чтобы перейти к настройкам группы, на странице &quot;Пользователи курса&quot; в нужной вам
+//               группе кликните по &quot;Шестерёнке&quot;. В загрузившемся окне появятся настройки группы.
+//             </p>
+//             <div className={styles.HelpCenterPage_FAQ_images}>
+//               <img src={groups} alt="Кнопка настроек" className={styles.HelpCenterPage_FAQ_images_firstStep} />
+//             </div>
+//             <br/>
+//             <h2> Настройки группы</h2>
+//             <p>
+//               Здесь вы можете выбрать настройки для группы: блокировать возможность отправки домашних заданий, строгая
+//               последовательность занятий, доступ пользователей группы к пользованию OVER AI, возможность получения пользователями
+//               сертификата после прохождения курса. Также, если вы хотите ограничить срок обучения по времени, можно
+//               установить продолжительность обучения в днях.
+//             </p>
+//             <div className={styles.HelpCenterPage_FAQ_images}>
+//               <img src={groups1} alt="Пользователи платформы" className={styles.HelpCenterPage_FAQ_images_firstStep} />
+//             </div>
+//             <br/>
+//             <h2> Настройка доступа к урокам</h2>
+//             <p>
+//               Внизу окна есть кнопка &quot;Показать все уроки&quot;, при нажатии отображаются все уроки курса,
+//               а также появляется возможность настройки доступа к урокам.
+//             </p>
+//             <div className={styles.HelpCenterPage_FAQ_images}>
+//               <img src={groups2} alt="Настройки таблицы" className={styles.HelpCenterPage_FAQ_images_firstStep} />
+//             </div>
+//           </div>
+//           <div className={styles.HelpCenterPage_banner}>
+//             <div className={styles.HelpCenterPage_banner_createProject}>
+//               <h1>Создайте свой проект на OVERSCHOOL прямо сейчас!</h1>
+//               <p>Попробуйте весь функционал в процессе использования и познай, насколько он удобен</p>
+//               <Button onClick={handleRegistrationUser} text={'Создать проект'} variant={'create'} />
+//             </div>
+//             <div className={styles.HelpCenterPage_banner_images}>
+//               <img src={firstStep} alt="Создать проект" className={styles.HelpCenterPage_banner_images_firstStep} />
+//               <img src={secondStep} alt="Создать проект" className={styles.HelpCenterPage_banner_images_secondStep} />
+//             </div>
+//           </div>
+//         </section>
+//       )
+//     }
+
+import { generatePath, useNavigate } from "react-router-dom";
+import { FC, memo, useState } from "react";
+import { Path } from "../../../enum/pathE";
+import styles from "../HelpPagesCommon.module.scss";
+import mainHelpStyles from "../HelpPage.module.scss";
+import { logo } from "../../../assets/img/common";
+import { Button } from "../../../components/common/Button/Button";
 import firstStep from '../../../assets/img/createProject/firstStep.png'
 import secondStep from '../../../assets/img/createProject/secondStep.png'
-import { useAppSelector } from '../../../store/hooks'
-import {selectUser} from '../../../selectors'
-import { Path } from 'enum/pathE'
-import { generatePath, useNavigate } from 'react-router-dom'
-import {logo} from "../../../assets/img/common";
-import styles from '../HelpPage.module.scss'
+import { InitPageHeader } from "../../Initial/newInitialPageHeader";
+import { IconSvg } from 'components/common/IconSvg/IconSvg'
+import { ArrowLeftIconPath } from '../../../assets/Icons/svgIconPath'
+import { Footer } from "../../../components/Footer/index";
+
 
 export const HelpGroupSettings = () => {
-      const navigate = useNavigate()
-    
-      const handleHelpPage = () => {
-        navigate(generatePath(Path.HelpPage))
-      }
-    
-      const handleLoginPage = () => {
-          navigate(generatePath(Path.LoginPage))
-      }
-    
-      const handleRegistrationUser = () => {
-          navigate(generatePath(Path.CreateSchool))
-      }
+  const navigate = useNavigate()
+  const [isLoginOpen, setLoginOpen] = useState(false);
+  const [isRegistrationOpen, setRegistrationOpen] = useState(false);
 
-      return (
-        <section className={styles.HelpCenterPage}>
-          <div className={styles.init_header}>
-               <a
-                  href={Path.InitialPage}
-                  className={styles.init_header_logo}
-                  style={{
-                     textDecoration: 'none',
-                     color: '#ba75ff',
-                     fontWeight: 'bold',
-                     padding: '0.5rem',
-                     borderRadius: '10px',
-                  }}
-                >
-                   <img src={logo} alt="Logotype ITOVERONE" />
-                   <p> IT OVERONE</p>
-                </a>
-                <div className={styles.header_block}>
-                   <Button onClick={handleHelpPage} variant={'logIn'} style={{ fontSize: '18px', fontWeight: '700' }} text={'Помощь'} />
-                   <Button onClick={handleLoginPage} variant={'logIn'} style={{ fontSize: '18px', fontWeight: '700' }} text={'Войти'} />
-                   <Button onClick={handleRegistrationUser} variant={'logIn'} style={{ fontSize: '18px', fontWeight: '700' }} text={'Создать платформу'} />
-                </div>
+  const handleHelpPage = () => {
+    navigate(generatePath(Path.HelpPage))
+  }
+
+  const handleLoginPage = () => {
+    navigate(generatePath(Path.LoginPage))
+  }
+
+  const handleRegistrationUser = () => {
+    navigate(generatePath(Path.CreateSchool))
+  }
+  return (
+    <div className={`${mainHelpStyles.helpPage} ${styles.helpPage}`}>
+      <div className={mainHelpStyles.bg}>
+        <div className={mainHelpStyles.bg_wrap1}></div>
+        <div className={mainHelpStyles.bg_wrap2}></div>
+        <div className={mainHelpStyles.bg_wrap3}></div>
+        <div className={mainHelpStyles.bg_wrap4}></div>
+      </div>
+
+      <InitPageHeader setLoginShow={setLoginOpen} setRegistrationShow={setRegistrationOpen} />
+
+      <div className={styles.sections}>
+        <div className={styles.section}>
+          <div className={styles.help_title_container}>
+            <div onClick={handleHelpPage} className={styles.back_btn}>
+              <IconSvg path={ArrowLeftIconPath} viewBoxSize="0 0 9 14" height={24} width={18} />
+            </div>
+            <p>Настройка группы</p>
+            <div></div>
           </div>
-          <div className={styles.HelpCenterPage_FAQ}>
-            <h1> Настройки группы </h1>
-            <h2> Переходим к настройкам группы</h2>
-            <p> 
-              Для того, чтобы перейти к настройкам группы, на странице &quot;Пользователи курса&quot; в нужной вам
-              группе кликните по &quot;Шестерёнке&quot;. В загрузившемся окне появятся настройки группы.
-            </p>
-            <div className={styles.HelpCenterPage_FAQ_images}>
-              <img src={groups} alt="Кнопка настроек" className={styles.HelpCenterPage_FAQ_images_firstStep} />
+        </div>
+
+
+
+        <div className={styles.section}>
+          <div className={styles.text_part}>
+            <div className={styles.section_title}>
+              <div className={styles.section_number}>1</div>
+              <h3 className={styles.section_title_text}>Переход к настройкам группы</h3>
             </div>
-            <br/>
-            <h2> Настройки группы</h2>
-            <p>
-              Здесь вы можете выбрать настройки для группы: блокировать возможность отправки домашних заданий, строгая
-              последовательность занятий, доступ пользователей группы к пользованию OVER AI, возможность получения пользователями
-              сертификата после прохождения курса. Также, если вы хотите ограничить срок обучения по времени, можно
-              установить продолжительность обучения в днях.
-            </p>
-            <div className={styles.HelpCenterPage_FAQ_images}>
-              <img src={groups1} alt="Пользователи платформы" className={styles.HelpCenterPage_FAQ_images_firstStep} />
-            </div>
-            <br/>
-            <h2> Настройка доступа к урокам</h2>
-            <p>
-              Внизу окна есть кнопка &quot;Показать все уроки&quot;, при нажатии отображаются все уроки курса,
-              а также появляется возможность настройки доступа к урокам.
-            </p>
-            <div className={styles.HelpCenterPage_FAQ_images}>
-              <img src={groups2} alt="Настройки таблицы" className={styles.HelpCenterPage_FAQ_images_firstStep} />
-            </div>
+            <p className={styles.section_text}>{`Для того, чтобы перейти к настройкам группы, 
+            на странице "Пользователи курса" в нужной вам группе кликните по "Шестерёнке". В 
+            загрузившемся окне появятся настройки группы.`}</p>
           </div>
-          <div className={styles.HelpCenterPage_banner}>
-            <div className={styles.HelpCenterPage_banner_createProject}>
-              <h1>Создайте свой проект на OVERSCHOOL прямо сейчас!</h1>
-              <p>Попробуйте весь функционал в процессе использования и познай, насколько он удобен</p>
-              <Button onClick={handleRegistrationUser} text={'Создать проект'} variant={'create'} />
-            </div>
-            <div className={styles.HelpCenterPage_banner_images}>
-              <img src={firstStep} alt="Создать проект" className={styles.HelpCenterPage_banner_images_firstStep} />
-              <img src={secondStep} alt="Создать проект" className={styles.HelpCenterPage_banner_images_secondStep} />
-            </div>
+          <div className={styles.img_part}>
+            <img
+              src={require("../../../assets/img/help/go_to_groups_settings.png")}
+              alt="К настройкам группы"
+            />
           </div>
-        </section>
-      )
-    }
+        </div>
+
+        <div className={styles.section}>
+          <div className={styles.img_part}>
+            <img
+              src={require("../../../assets/img/help/groups_settings.png")}
+              alt="Настройки группы"
+            />
+          </div>
+          <div className={styles.text_part}>
+            <div className={styles.section_title}>
+              <div className={styles.section_number}>2</div>
+              <h3 className={styles.section_title_text}>Настройки группы</h3>
+            </div>
+            <p className={styles.section_text}>{`Здесь вы можете выбрать настройки для группы: блокировать 
+            возможность отправки домашних заданий, строгая последовательность занятий, доступ пользователей
+             группы к пользованию OVER AI, возможность получения пользователями сертификата после прохождения
+              курса. Также, если вы хотите ограничить срок обучения по времени, можно установить продолжительность обучения в днях.`}</p>
+          </div>
+        </div>
+
+
+        <div className={styles.section}>
+          <div className={styles.text_part}>
+            <div className={styles.section_title}>
+              <div className={styles.section_number}>3</div>
+              <h3 className={styles.section_title_text}>Настройка доступа к урокам</h3>
+            </div>
+            <p className={styles.section_text}>{`Внизу окна есть кнопка "Показать  все уроки"
+          , при нажатии отображаются все уроки курса, а также появляется  возможность настройки
+           доступа к урокам.`}</p>
+          </div>
+          <div className={styles.img_part}>
+            <img
+              src={require("../../../assets/img/help/access_groups_settings.png")}
+              alt="Доступ к урокам"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className={mainHelpStyles.ctaBlock}>
+        <div className={mainHelpStyles.ctaText}>
+          <h2>Создайте свой проект на Course Hub прямо сейчас!</h2>
+          <p>
+            Попробуйте весь функционал в процессе использования и узнайте, как
+            удобно работать на нашей платформе.
+          </p>
+          <Button
+            text="Попробовать бесплатно"
+            variant="newLeaveRequest"
+            onClick={handleRegistrationUser}
+          />
+        </div>
+        <div className={mainHelpStyles.ctaImage}>
+          <img
+            src={require("../../../assets/img/common/cta-image.png")}
+            alt="CTA-изображение"
+          />
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+
+  )
+}
