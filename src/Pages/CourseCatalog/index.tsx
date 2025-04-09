4d542004a37d5b13cde5bd6781701114d26027e9
overschool2 <144776685+overschool2@users.noreply.github.com>
2025-04-09 12:59:57 +0300

    Revert "Yuliya sokolova"

--- src/Pages/CourseCatalog/index.tsx
+++ src/Pages/CourseCatalog/index.tsx
@@ -1,19 +1,15 @@
 import { ChangeEvent, FC, memo, useEffect, useState } from 'react'
 import styles from './CourseCatalog.module.scss'
-import { Card, CardActionArea, CardContent, CardMedia, InputBase, Pagination, Typography, alpha, styled, Box } from '@mui/material'
+import { Card, CardActionArea, CardContent, CardMedia, InputBase, Pagination, Typography, alpha, styled } from '@mui/material'
 import { useFetchCourseCatalogQuery, useFilteredSearchMutation } from 'api/catalogServices'
 import { SimpleLoader } from 'components/Loaders/SimpleLoader'
+import SearchIcon from '@mui/icons-material/Search'
 import { Path } from 'enum/pathE'
 import { motion } from 'framer-motion'
 import { CatalogResponseT } from 'api/apiTypes'
 import { generatePath, useNavigate } from 'react-router-dom'
-import { BackgroundAnimation } from 'components/BackgroundAnimation'
-import { SearchIcon } from 'components/SearchBar/svgIconsPath'
-import { studentsSearchPath } from 'components/AllStudentsBlock/config/svgIconsPath'
-import { IconSvg } from 'components/common/IconSvg/IconSvg'
-import { Button } from 'components/common/Button/Button'
-import { InitPageHeader } from 'Pages/Initial/newInitialPageHeader'
-import { Footer } from 'components/Footer/index'
+import { logo } from 'assets/img/common'
+import courseBack from 'assets/img/course/qa.jpg'
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
   borderRadius: theme.shape.borderRadius,
@@ -26,11 +22,22 @@ const Search = styled('div')(({ theme }) => ({
     width: 'auto',
   },
 }))
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}))
+
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'inherit',
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: '8px',
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('md')]: {
@@ -38,12 +45,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     },
   },
 }))
-
-const colors: string[] = [
-  '#93CEF3', '#93A6F3', '#A693F3', '#D693F3',
-  '#ED93F3', '#F393B8', '#F3BD93', '#C6F393',
-  '#93A6F3', '#D5F393', '#93CEF3', '#F393E8'
-];
 
 export const CourseCatalogPage: FC = memo(() => {
   const navigate = useNavigate()
@@ -64,25 +65,11 @@ export const CourseCatalogPage: FC = memo(() => {
     }
   }, [coursesData])
 
-  const [searchValue, setSearchValue] = useState('');
-  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
   const handleFilter = async (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const value = e.target.value;
-    setSearchValue(value)
-    timeoutId && clearTimeout(timeoutId);
-
-    const newTimeout = setTimeout(async () => {
-      if (value.trim()) {
-        const data = await filterCourses(value).unwrap();
-        setCourses(data);
-      } else {
-        const freshData = await refetch().unwrap();
-        setCourses(freshData);
-      }
-    }, 500);
-    setTimeoutId(newTimeout);
+    await filterCourses(e.target.value)
+      .unwrap()
+      .then(data => setCourses(data))
   }
-
 
   if (!courses) {
     return (
@@ -107,44 +94,33 @@ export const CourseCatalogPage: FC = memo(() => {
       }}
       className={styles.wrapper}
     >
-      <InitPageHeader />
-      <BackgroundAnimation />
       <div className={styles.wrapper_body}>
         <Typography gutterBottom variant="h5" sx={{ width: '100%', textAlign: 'center' }} color={'#ba75ff'} component="div">
           <p className={styles.wrapper_body_textHeader}>
-            Каталог курсов
+            <p>Каталог курсов онлайн-школ на платформе </p>
+            <a
+              href={Path.InitialPage}
+              className={styles.headerButton}
+              style={{
+                textDecoration: 'none',
+                color: '#ba75ff',
+                fontWeight: 'bold',
+                padding: '0.5rem',
+                border: '2px solid #ba75ff',
+                borderRadius: '7px',
+              }}
+            >
+              <img src={logo} alt="Logotype ITOVERONE" />
+              <p>Overschool.by</p>
+            </a>
           </p>
         </Typography>
         <div className={styles.search}>
-          <Button
-            onClick={async () => {
-              setPage(1);
-              setSearchValue('');
-              const data = await refetch().unwrap();
-              setCourses(data);
-            }}
-            variant='newPrimary'
-            className={styles.wrapper_body_courses_button}
-            text={''}
-          >
-            Все курсы
-          </Button>
-          <Search
-            className={styles.search_field}
-            sx={{
-              border: '1px solid gray',
-              display: 'flex',
-              alignItems: 'center',
-              gap: '8px',
-              borderRadius: '10px',
-              padding: '5px 10px',
-              backgroundColor: 'transparent'
-            }}
-          >
-              <IconSvg width={30} height={30} viewBoxSize="0 0 30 30" path={studentsSearchPath}>
-                <SearchIcon />
-              </IconSvg>
-            <StyledInputBase value={searchValue} onChange={handleFilter} placeholder="Поиск по курсам…" inputProps={{ 'aria-label': 'поиск' }} sx={{ width: '100%', paddingLeft: '8px' }} />
+          <Search className={styles.search_field} sx={{ border: '1px solid gray', borderRadius: '10px', backgroundColor: 'transparent' }}>
+            <SearchIconWrapper>
+              <SearchIcon sx={{ color: '#ba75ff' }} />
+            </SearchIconWrapper>
+            <StyledInputBase onChange={handleFilter} placeholder="Поиск по курсам…" inputProps={{ 'aria-label': 'поиск' }} sx={{ width: '100%' }} />
           </Search>
         </div>
         {isFetching || isLoading ? (
@@ -152,55 +128,24 @@ export const CourseCatalogPage: FC = memo(() => {
         ) : courses.results.length ? (
           <div className={styles.wrapper_body_courses}>
             {courses.results.map((course: any, index: number) => (
-              <Card className={styles.wrapper_body_courses_card} sx={{ cursor: 'pointer', transition: 'all ease-in 0.2s', backgroundColor: '#CFE2FF'}} key={index}>
-                <div className={styles.colorCircle} style={{ backgroundColor: colors[index % colors.length] }}></div>
-                  <CardActionArea
-                    disableRipple
-                    sx={{
-                      backgroundColor: "transparent",
-                      "& .MuiCardActionArea-focusHighlight": {
-                        backgroundColor: "rgba(0, 0, 0, 0) !important"
-                      },
-                      height: '100%'
-                    }}
-                    onClick={() => navigate(generatePath(Path.Catalog + Path.CatalogCourse, { courseId: course.course_id }))}
-                  >
-                    <CardContent sx={{ display:'flex', height: '100%' }}>
-                      <Box
-                        sx={{
-                          width: '60%',
-                          display: 'flex',
-                          alignItems: 'flex-start',
-                          justifyContent: 'flex-start',
-                        }}
-                      >
-                        <Typography gutterBottom color="#000000" component="div" className={styles.wrapper_body_courses_card_name}>
-                          {course.name}
-                        </Typography>
-                      </Box>
-                      <Box
-                        sx={{
-                          width: '40%',
-                          height: '90%',
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                        }}
-                      >
-                        <CardMedia
-                          component="img"
-                          height="40"
-                          image={course.photo}
-                          alt={course.name}
-                          sx={{
-                            width: '85%',
-                            height: 'max-content'
-                          }}
-                        />
-                      </Box>
-                    </CardContent>
-                  </CardActionArea>
+              <Card className={styles.wrapper_body_courses_card} sx={{ cursor: 'pointer', transition: 'all ease-in 0.2s' }} key={index}>
+                <CardActionArea onClick={() => navigate(generatePath(Path.Catalog + Path.CatalogCourse, { courseId: course.course_id }))}>
+                  <CardMedia component="img" height="240" image={course.photo} alt={course.name} />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" color={'#ba75ff'} component="div">
+                      <strong>{course.name}</strong>
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
               </Card>
+              // <Card
+              //   style={{ background: `url(${courseBack}` }}
+              //   className={styles.wrapper_body_courses_card}
+              //   sx={{ cursor: 'pointer', transition: 'all ease-in 0.2s' }}
+              //   key={index}
+              // >
+              //   <div className={styles.wrapper_body_courses_card_name}>{course.name}</div>
+              // </Card>
             ))}
           </div>
         ) : (
@@ -211,7 +156,6 @@ export const CourseCatalogPage: FC = memo(() => {
 
         <Pagination count={Math.ceil(courses.count / 12)} page={currentPage} sx={{ color: '#ba75ff' }} onChange={(event, page) => setPage(page)} />
       </div>
-      <Footer />
     </motion.div>
   )
 })
