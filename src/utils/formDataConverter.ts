8d395f6e43b555ef7b176a9752749954b268d951
1BalaN <gormachdv@gmail.com>
2025-04-10 13:38:53 +0300

    вернул в исходное состоняие

--- src/utils/formDataConverter.ts
+++ src/utils/formDataConverter.ts
@@ -1,18 +1,8 @@
 export const formDataConverter = (data: object) => {
-  const formdata = new FormData();
+  const formdata = new FormData()
 
   Object.entries(data).forEach(([key, value]) => {
-    if (Array.isArray(value)) {
-      value.forEach((item, index) => {
-        formdata.append(`${key}[${index}]`, String(item)); 
-      });
-    } else if (typeof value === 'number') {
-      formdata.append(key, String(value)); 
-    } else {
-      formdata.append(key, value as string); 
-    }
-  });
-
-  return formdata;
-};
-
+    formdata.append(key, value)
+  })
+  return formdata
+}
