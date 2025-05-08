f90c1569ea6a6897c40e9e71babbcbb91d027f43
aleksejpetrovec <rbtechologics@mail.ru>
2025-05-08 13:00:19 +0300

    upd social auth;

--- src/ServicePages/SocialAuthPage/index.tsx
+++ src/ServicePages/SocialAuthPage/index.tsx
@@ -1,17 +1,22 @@
-import { useAuthSocialQuery } from 'api/userLoginService'
+import { useFetchConfiguredDomainsQuery } from 'api/DomainService'
+import { useGetSchoolsMutation } from 'api/getSchoolService'
+import { useAuthSocialQuery, useLazyGetUserInfoQuery } from 'api/userLoginService'
 import { LoaderLayout } from 'components/Loaders/LoaderLayout'
 import { Path } from 'enum/pathE'
+import { RoleE } from 'enum/roleE'
+import { SchoolT } from 'Pages/ChooseSchool/ChooseSchool'
 import { FC, useEffect } from 'react'
 import { generatePath, useNavigate } from 'react-router-dom'
 import { selectUser } from 'selectors'
 import { useAppDispatch, useAppSelector } from 'store/hooks'
-import { authState, id } from 'store/redux/users/slice'
+import { auth, authState, id, role, userName } from 'store/redux/users/slice'
 
 export const SocialAuthPage: FC = () => {
-  const { data: authData } = useAuthSocialQuery()
+  const { data: authData, isSuccess, isFetching: isLoading } = useAuthSocialQuery()
   const dispatch = useAppDispatch()
   const { auth: authetificationState } = useAppSelector(selectUser)
   const navigate = useNavigate()
+  const [getUserInfo] = useLazyGetUserInfoQuery()
 
   useEffect(() => {
     if (authData) {
@@ -26,6 +31,18 @@ export const SocialAuthPage: FC = () => {
       navigate(generatePath(Path.ChooseSchool))
     }
   }, [authetificationState])
+
+  useEffect(() => {
+    if (isSuccess) {
+      getUserInfo()
+        .unwrap()
+        .then(resp => {
+          dispatch(auth(true))
+          dispatch(userName(resp[0]?.username))
+        })
+        .catch(() => console.log('Error fetching user data'))
+    }
+  }, [isSuccess, isLoading])
 
   return <LoaderLayout></LoaderLayout>
 }
